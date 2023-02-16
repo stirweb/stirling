e25b9084bbd7358eec83b5c60085f167a5993aa6
Ryan Kaye <ryan.kaye@stir.ac.uk>
2023-02-16 20:36:48 +0000

    QA protect Script: Fix url bug

--- medias/Categorised/Dist/js/app.js
+++ medias/Categorised/Dist/js/app.js
@@ -8232,13 +8232,13 @@ var stir = stir || {};
 (function () {
   // if we are in preview, dynamically load the preview tools
   // otherwise just skip this
-  console.log(window.location.hostname);
+  console.log(window.location.hostname + "1");
   switch (window.location.hostname) {
     case "localhost":
       stir.addScript("/src/js-other/t4-preview-tools.js");
       break;
     case "stirweb.github.io":
-      stir.addScript("/src/js-other/qa-protect.js");
+      stir.addScript("/stirling/src/js-other/qa-protect.js");
       break;
     case "stiracuk-cms01-production.terminalfour.net":
       //stir.addScript('<t4 type="media" id="158095" formatter="javascript/*" />');
