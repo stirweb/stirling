3ec14ac1a0be29161507b69b4c523f4c43cc89d8
Robert Morrison <r.w.morrison@stir.ac.uk>
2023-02-22 12:14:15 +0000

    add Virtual Tour files

---
+++ medias/Categorised/Dist/bundles/virtual-tour/js/vod-jsonp.js
@@ -0,0 +1,81 @@
+callback([{
+  "id": 52116,
+  "title": "Andrew Miller Building",
+  "about": "<p>The central hub of the campus where it’s all happening! It houses our spacious library, cafes, shops, a bank, a pharmacy and a prayer room. It’s also a great meeting spot. The Atrium often plays host to an ever-changing line up of specialist stalls – from pop-up food to mini record fairs.</p>\n<ul>\n<li><a href=\"/student-life/campus-facilities/the-atrium/\">The atrium</a></li>\n<li><a href=\"/student-life/campus-facilities/the-library/\">The library</a></li>\n<li><a href=\"/student-life/campus-facilities/campus-transformation/\">Campus transformation</a></li>\n</ul>",
+  "coord": "42, 15",
+  "image": "/media/stirling/global-assets/campus-life/small/5197717243_7291764a99_o.jpg",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/cAE1jHeU4rw\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": "<iframe allowfullscreen frameborder=0 src=\"https://www.stir.ac.uk/archive/360/html5/virtual-tour/cafe.html\"></iframe>"
+},{
+  "id": 52117,
+  "title": "The Students&rsquo; Union",
+  "about": "<p>Our Students’ Union is here to make student life at Stirling the best it can be. It’s home to over 120 clubs and societies, along with student services and support. There are great places to meet up and grab some food or a drink. It also hosts fun night-time events including DJ nights, concerts and even a Jurassic Party!</p>\n<ul>\n<li><a href=\"/student-life/students-union/\">Students&rsquo; Union</a></li>\n<li><a href=\"https://www.stirlingstudentsunion.com/intheunion/whatson/ \">Find out what’s on</a></li>\n<li><a href=\"/student-life/students-union/clubs-societies/\">Clubs and societies</a></li>\n</ul>",
+  "coord": "37, 13",
+  "image": "/media/stirling/global-assets/campus-life/small/GRA_1628_1-(1).jpg",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/NYHdv4UH9dI\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": "<iframe allowfullscreen frameborder=0 src=\"https://www.stir.ac.uk/archive/360/html5/virtual-tour/bar.html\"></iframe>"
+},{
+  "id": 52118,
+  "title": "Macrobert Arts Centre",
+  "about": "<p>You can enjoy movies, live theatre and much more right on your doorstep. Our cinema shows everything from the latest blockbusters to independent gems, and students get great discounts! Experience live theatre in our 500-seat auditorium, with shows from stand-up comedy to gripping drama and boundary-pushing dance.</p>\n<ul>\n<li><a href=\"https://macrobertartscentre.org/\">Macrobert Arts Centre</a></li>\n<li><a href=\"https://macrobertartscentre.org/whatson/\">Find out what\'s on&nbsp;</a></li>\n</ul>",
+  "coord": "46, 16",
+  "image": "/media/stirling/global-assets/campus-life/small/students-macrobert-arts-centre-578x358.jpg",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/UeZ6u3pwdFw\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": ""
+},{
+  "id": 52134,
+  "title": "Cottrell Building",
+  "about": "<p>Home to some of our modern study facilities including lecture theatres, seminar rooms and psychology, computing and science labs. Our study pods are a good place to meet up with your course mates for group work. You’ll also find the Student Services Hub – your one stop shop for support with every aspect of student life.</p>\n<ul>\n<li><a href=\"/study/\">Study at Stirling</a></li>\n<li><a href=\"/student-life/support-wellbeing/\">Student Services Hub</a></li>\n</ul>",
+  "coord": "54, 22",
+  "image": "/media/stirling/global-assets/people/small/Postgraduate-student-studying-finance578x358.jpg",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/1b83t3LVwZc\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": ""
+},{
+  "id": 52135,
+  "title": "INTO Building",
+  "about": "<p>Our newest building, opened in 2017 by Prince Edward, Earl of Wessex. Here international students study a range of foundation or pre-sessional English courses. You’ll find state-of-the-art facilities from lecture halls to classrooms, as well as our Umami cafe serving fantastic noodles and curries.</p>\n<ul>\n<li><a href=\"/international/international-students/pathway-programmes/about-into/\">INTO University of Stirling</a></li>\n<li><a href=\"/international/international-students/pathway-programmes/\">Pathway Programmes</a></li>\n<li><a href=\"/international/international-students/pre-sessional-english-language-courses/\">Pre-sessional English Courses</a></li>\n</ul>",
+  "coord": "60, 16",
+  "image": "/media/stirling/global-assets/campus-life/small/INTO-Building-578x358.jpg",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/uJ713_F_WU0\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": ""
+},{
+  "id": 52136,
+  "title": "Sports Facilities",
+  "about": "<p>We\'re Scotland\'s University for Sporting Excellence. Our sports facilities are first class with everything from multi-purpose outdoor pitches, a 9-hole golf course to an indoor gym and 50 metre swimming pool. There’s something for everyone to enjoy!</p>\n<ul>\n<li><a href=\"/student-life/sport-at-stirling/\">Sport and wellbeing</a></li>\n<li><a href=\"https://www.stir.ac.uk/courses/?collection=stir-courses&amp;query=sport \">Study sport at Stirling</a></li>\n<li><a href=\"/student-life/sport-at-stirling/performance-sport/\">Performance sport</a></li>\n<li><a href=\"/student-life/sport-at-stirling/facilities/facilities-redevelopment/\">Sport transformation</a></li>\n</ul>",
+  "coord": "55, 40",
+  "image": "/media/stirling/global-assets/people/small/YEL_5202-(4).JPG",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/BwH2WcwQvi8\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": ""
+},{
+  "id": 52137,
+  "title": "Pathfoot Building",
+  "about": "<p>The very first building completed on campus in 1967. Alongside lecture theatres, classrooms, and a tropical aquarium lab, Pathfoot is also home to the University\'s permanent art collection. It hosts many exhibitions in its main concourse and corridors, Crush Hall and seventeen courtyards.</p>\n<ul>\n<li><a href=\"/study/\">Study at Stirling</a></li>\n<li><a href=\"/about/faculties-and-services/art-collection/\">The Art Collection</a></li>\n</ul>",
+  "coord": "33, 70",
+  "image": "/media/stirling/global-assets/campus-life/small/pathfoot-578x358.jpg",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/mRRHaxX2iU0\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": "<iframe allowfullscreen frameborder=0 src=\"https://www.stir.ac.uk/archive/360/html5/virtual-tour/lab-2.html\"></iframe>"
+},{
+  "id": 52138,
+  "title": "Accommodation",
+  "about": "<p>Your home away from home. From hillside chalets to self-catered flats, we’ve got something to fit every budget and each individual – both on and off-campus. You’ll feel safe and secure with our student Accommodation Liaison Officers and security team on hand 24/7.</p>\n<ul>\n<li><a href=\"/student-life/accommodation/\">Find your residence</a></li>\n<li><a href=\"/student-life/accommodation/safety-security/\">Safety and security</a></li>\n</ul>",
+  "coord": "20, 35",
+  "image": "/media/stirling/global-assets/accommodation/willow-court-studio-flats/thumbnails/beech-court-studio-bedroom-578x358.jpg",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/alNcx-z0yVk\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": "<iframe allowfullscreen frameborder=0 src=\"https://www.stir.ac.uk/archive/360/html5/virtual-tour/alexander-court.html\"></iframe>"
+},{
+  "id": 52151,
+  "title": "Sights on Campus",
+  "about": "<p>At Stirling you’re always surrounded by breathtaking views and an incredible landscape. The Loch Bridge, linking our accommodation to the heart of the campus, gives the best views of the Ochil Hills and the Wallace Monument. Our campus is pretty unique as it also includes an 18th century castle!</p>\n<ul>\n<li><a href=\"/student-life/campus-facilities/the-loch/\">Airthrey Loch and Castle</a></li>\n<li><a href=\"https://blog.stir.ac.uk/eight-cultural-gems-to-explore-on-campus-and-beyond/\">Hidden campus gems</a></li>\n</ul>",
+  "coord": "28, 22",
+  "image": "/media/stirling/global-assets/landscape/small/people-walking-around-the-loch578x358.JPG",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Tf8X4Hhv7_Q\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": "<iframe allowfullscreen frameborder=0 src=\"https://www.stir.ac.uk/archive/360/html5/virtual-tour/loch.html\"></iframe>"
+},{
+  "id": 52152,
+  "title": "Bridge of Allan and Stirling City Centre",
+  "about": "<p>With a population of almost 100,000, Stirling provides a great community for city life with high street shops and quirky pop-ups, restaurants, cafes and clubs.&nbsp; We’re proud to call Stirling our home – it’s a city shaped by its unique history, with a lively arts scene and the beauty of a national park close by.</p>\n<ul>\n<li><a href=\"/student-life/city-of-stirling/\">Discover Stirling</a></li>\n<li><a href=\"/student-life/explore-scotland/\">Explore Scotland</a></li>\n</ul>",
+  "coord": "68.5, 74.8",
+  "image": "/media/stirling/global-assets/stirling-city-life/small/Students-walking-in-Stirling-city-centre-7.jpg",
+  "video": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/SZf3yvfj0hM\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+  "vr360": ""
+},{}]);
