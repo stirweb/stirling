ed48a0edfe41275a3651cde44d5448c557f4879d
Robert Morrison <r.w.morrison@stir.ac.uk>
2023-02-28 12:21:29 +0000

    fix loupe script URL for t4

--- medias/Categorised/Dist/js/app.js
+++ medias/Categorised/Dist/js/app.js
@@ -6396,8 +6396,11 @@ stir.lazy(document.querySelectorAll('.stirlazy,[data-lazy-container]'));
     case "localhost":
       stir.addScript('/src/js-other/loupe.js');
       break;
+    case "www.stir.ac.uk":
+    case "stiracuk-cms01-production.terminalfour.net":
+      stir.addScript('<t4 type="media" id="158075" formatter="path/*" />');
+      break;
     default:
-      stir.addScript('/webcomponents/dist/js/other/loupe.js');
       break;
   }
 })();
--- src/js-impl/loupe.js
+++ src/js-impl/loupe.js
@@ -2,6 +2,10 @@
 	if(!document.querySelector(".c-zoom")) return;
 	switch (window.location.hostname) {
 		case "localhost": stir.addScript('/src/js-other/loupe.js'); break;
-		default: stir.addScript('/webcomponents/dist/js/other/loupe.js'); break;
+		case "www.stir.ac.uk":
+		case "stiracuk-cms01-production.terminalfour.net":
+			stir.addScript('<t4 type="media" id="158075" formatter="path/*" />'); break;
+		default:
+			break;
 	}
 })();
