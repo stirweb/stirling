23a6f29713344f7d7356860031a771e1e70c8830
Robert Morrison <r.w.morrison@stir.ac.uk>
2023-04-06 11:30:43 +0100

    update Altmetric script to Explorer API

--- medias/Categorised/Dist/js/other/_unminified/altmetric.js
+++ medias/Categorised/Dist/js/other/_unminified/altmetric.js
@@ -1,42 +1,39 @@
 var stir = stir || {};
 stir.altmetric = {
-  key: '64d04a1774ca2eb54316de3a8365cda2',
+  url: function () {
+    if (UoS_env.name === "dev") return 'altmetric.json';
+    return '<t4 type="media" id="166310" formatter="path/*" />';
+  }(),
   max: 5,
+  donut: function donut(doi) {
+    return '<div class=altmetric-embed data-badge-type=donut data-badge-popover=left data-doi="' + doi + '" data-hide-no-mentions=true></div>';
+  },
+  handle: stir.curry(function (title, handle, index) {
+    return "<a href=\"http://hdl.handle.net/".concat(handle, "\">").concat(index === 0 ? title : handle, "</a>");
+  }),
+  handles: function handles(title, _handles) {
+    return _handles.map(stir.altmetric.handle(title)).shift();
+  },
+  row: function row(element, index) {
+    return "<tr><td>".concat(index + 1, "</td><td>").concat(stir.altmetric.handles(element.attributes['title'], element.attributes.identifiers['handles']), "</td><td class=donut>").concat(element.attributes.identifiers['dois'].map(stir.altmetric.donut).join(''), "</td></tr>");
+  },
+  tbody: function tbody(json) {
+    return json.slice(0, stir.altmetric.max).map(stir.altmetric.row).join('');
+  },
+  table: function table(json) {
+    return "<table><caption>University of Stirling research papers ranked by Altmetric</caption><thead><tr><th scope=col>Position</th><th scope=col>Research paper</th><th class=donut scope=col>Altmetric score</th></thead></tr><tbody>".concat(stir.altmetric.tbody(json), "</tbody></table>");
+  },
   callback: function callback(json) {
-    var index;
-    var handle;
-    var doi;
-    var html = ['<table>', '<caption>University of Stirling research papers ranked by Altmetric</caption>', '<thead><tr>', '<th scope=col>Position</th><th scope=col>Research paper</th><th class=donut scope=col>Altmetric score</th>', '</thead></tr>', '<tbody>'];
+    if (!json) return;
     var container = document.getElementById('top10');
-    for (index = 0; index < json.results.length; ++index) {
-      handle = 'http://hdl.handle.net/' + json.results[index]['handle'];
-      doi = json.results[index]['doi'];
-      html.push('<tr><td>');
-      html.push((index + 1).toString());
-      html.push('</td><td>');
-      html.push('<a href="' + handle + '">' + json.results[index]['title'] + '</a>');
-      html.push('</td>' + '<td class=donut>');
-      html.push('<div class=altmetric-embed data-badge-type=donut data-badge-popover=left data-doi="' + doi + '" data-hide-no-mentions=true></div>');
-      html.push('</td></tr>');
+    if (!container) {
+      return console.warn('[Altmetric] DOM container not found');
     }
-    html.push('</tbody>');
-    html.push('</table>');
-    container && (container.innerHTML = html.join(''));
-
-    // call Altmetric's embed function:
-    if ('function' == typeof _altmetric_embed_init) {
-      _altmetric_embed_init(container);
-    }
+    container.innerHTML = stir.altmetric.table(json);
   }
 };
 
 /* Get the feed */
 (function () {
-  var query,
-    el = document.createElement('script');
-  query = 'key=' + stir.altmetric.key + '&num_results=' + stir.altmetric.max;
-  query += '&group=stirling&order_by=score&callback=stir.altmetric.callback';
-  query += '&citation_type=news%2Carticle%2Cclinical_trial_study_record%2Cdataset%2Cbook%2Cchapter%2Cgeneric';
-  el.src = 'https://api.altmetric.com/v1/citations/at?' + query;
-  document.body.insertAdjacentElement('beforeend', el);
+  stir.getJSON(stir.altmetric.url, stir.altmetric.callback);
 })();
