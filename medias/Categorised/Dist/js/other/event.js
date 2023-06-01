4aada0ad60943ece3bda5c6281cd88a9b9a4ac3a
Ryan Kaye <ryankaye@gmail.com>
2023-06-01 13:23:50 +0100

    Events revamp: Event series listing

--- medias/Categorised/Dist/js/other/event.js
+++ medias/Categorised/Dist/js/other/event.js
@@ -1,1 +1,12 @@
-!function(){var e=document.head.querySelector("[name=sid][content]");if(e)for(var t=document.querySelectorAll('a[data-section-id="'+e.getAttribute("content")+'"][data-type="event"]'),n=0;n<t.length;n++)t[n].parentNode.removeChild(t[n])}(),function(){var e=stir.node("#shareurl"),e=(e&&(e.value=window.location.href),stir.node("#copyurl"));e&&e.addEventListener("click",e=>(async()=>{await navigator.clipboard.writeText(window.location.href)})())}();
+!function(){var t=document.head.querySelector("[name=sid][content]");if(t)for(var e=document.querySelectorAll('a[data-section-id="'+t.getAttribute("content")+'"][data-type="event"]'),i=0;i<e.length;i++)e[i].parentNode.removeChild(e[i])}(),function(t){if(t){t&&(t.value=window.location.href);t=stir.node("#copyurl");t&&t.addEventListener("click",t=>(async()=>{await navigator.clipboard.writeText(window.location.href)})())}}(stir.node("#shareurl")),function(t){if(t){const i=t,e=i.dataset&&i.dataset.seriesid?i.dataset.seriesid:null,n=(t,e)=>t?`<h3 class="header-stripped ${e}">${t}</h3>`:"";const s=stir.map((t,e)=>`
+        <div class="${e%2==1?"":"u-bg-grey"} ${0===e?"u-heritage-line-top u-border-width-5":""} u-p-1 c-event-list u-gap">
+          <div class="u-w-500">
+            <span class="u-inline-block u-mb-1"><strong>Event</strong><br />
+            <a href="${t.url}">${t.title}</a></span><br />
+            <strong >Date:</strong> ${t.stirStart} <br />
+            <strong>Time:</strong> ${t.startTime} - ${t.endTime}
+          </div>
+          <div><span class="u-inline-block u-mb-1"><strong>Description</strong><br />${t.summary} </div>
+          <div><span class="u-inline-block u-mb-1"><strong>Audience</strong><br />${t.isPublic?"Public":"Staff / students"} </div>
+          <div>${t.recording?'<span class="u-inline-block u-mb-1"><strong>Recording</strong><br />Available':""} </div>
+        </div>`),r=stir.join(""),o=()=>{var t=new Date;return Number(t.toISOString().split("T")[0].split("-").join(""))};const a=stir.filter(t=>t.endInt>=o());const d=stir.filter(t=>t.endInt<o()),l=stir.filter(t=>t.isSeriesChild===e);const c=stir.curry((t,e)=>(stir.setHTML(t,e),!0));stir.getJSON("index.json",t=>{var e=stir.compose(r,s,a,l)(t),t=stir.compose(r,s,d,l)(t),e=e.length?n("Upcoming","")+e:"",t=t.length?n("Passed","u-mt-2")+t:"";c(i,e+t)})}}(stir.node("#seriesevents"));
--- src/js-other/event.js
+++ src/js-other/event.js
@@ -1,4 +1,6 @@
-// wrap everything in an IIFE so we don't pollute the Global Scope
+/*
+
+*/
 (function () {
   // get a reference to the Section ID meta tag
   var pageSID = document.head.querySelector("[name=sid][content]");
@@ -17,8 +19,16 @@
   }
 })();
 
-(function () {
-  const shareInput = stir.node("#shareurl");
+/*
+|
+|   SHARE BUTTONS
+|
+*/
+
+(function (scope) {
+  if (!scope) return;
+
+  const shareInput = scope;
 
   if (shareInput) shareInput.value = window.location.href;
 
@@ -29,4 +39,92 @@
   };
 
   copyUrlBtn && copyUrlBtn.addEventListener("click", (event) => copyUrl());
-})();
+})(stir.node("#shareurl"));
+
+/*
+|
+|   SERIES LISTING
+|
+*/
+
+(function (scope) {
+  if (!scope) return;
+
+  const seriesEventsArea = scope;
+  const seriesId = seriesEventsArea.dataset && seriesEventsArea.dataset.seriesid ? seriesEventsArea.dataset.seriesid : null;
+
+  /*
+    |
+    |   RENDERERS
+    |
+    */
+
+  const renderHeader = (text, classes) => (text ? `<h3 class="header-stripped ${classes}">${text}</h3>` : ``);
+
+  const renderEvent = (item, index) => {
+    return `
+        <div class="${index % 2 === 1 ? `` : `u-bg-grey`} ${index === 0 ? `u-heritage-line-top u-border-width-5` : ``} u-p-1 c-event-list u-gap">
+          <div class="u-w-500">
+            <span class="u-inline-block u-mb-1"><strong>Event</strong><br />
+            <a href="${item.url}">${item.title}</a></span><br />
+            <strong >Date:</strong> ${item.stirStart} <br />
+            <strong>Time:</strong> ${item.startTime} - ${item.endTime}
+          </div>
+          <div><span class="u-inline-block u-mb-1"><strong>Description</strong><br />${item.summary} </div>
+          <div><span class="u-inline-block u-mb-1"><strong>Audience</strong><br />${item.isPublic ? `Public` : `Staff / students`} </div>
+          <div>${item.recording ? `<span class="u-inline-block u-mb-1"><strong>Recording</strong><br />Available` : ``} </div>
+        </div>`;
+  };
+
+  /*
+    |
+    |   HELPERS
+    |
+    */
+
+  const renderEventsMapper = stir.map(renderEvent);
+
+  const joiner = stir.join("");
+
+  const getNow = () => {
+    let yourDate = new Date();
+    return Number(yourDate.toISOString().split("T")[0].split("-").join(""));
+  };
+
+  const isUpcoming = (item) => item.endInt >= getNow();
+
+  const isUpcomingFilter = stir.filter(isUpcoming);
+
+  const isPassed = (item) => item.endInt < getNow();
+
+  const isPassedFilter = stir.filter(isPassed);
+
+  const isSeriesChildFilter = stir.filter((item) => item.isSeriesChild === seriesId);
+
+  const getJSONUrl = () => {
+    return "index.json";
+  };
+
+  const setDOMContent = stir.curry((node, html) => {
+    stir.setHTML(node, html);
+    return true;
+  });
+
+  /*
+    |
+    |   ONLOAD
+    |
+    */
+
+  const url = getJSONUrl();
+
+  stir.getJSON(url, (initialData) => {
+    const seriesUpcomingData = stir.compose(joiner, renderEventsMapper, isUpcomingFilter, isSeriesChildFilter)(initialData);
+    const seriesPastData = stir.compose(joiner, renderEventsMapper, isPassedFilter, isSeriesChildFilter)(initialData);
+
+    const upcomingHtml = seriesUpcomingData.length ? renderHeader("Upcoming", "") + seriesUpcomingData : ``;
+    const pastHtml = seriesPastData.length ? renderHeader("Passed", "u-mt-2") + seriesPastData : ``;
+
+    setDOMContent(seriesEventsArea, upcomingHtml + pastHtml);
+  });
+})(stir.node("#seriesevents"));
