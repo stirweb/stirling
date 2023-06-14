dcfe0f667094a0d75b0d4aa93e0bc4e65208d12b
Ryan Kaye <ryankaye@gmail.com>
2023-06-14 13:00:20 +0100

    Welcome events: Pagination + upcoming only

--- medias/Categorised/Dist/js/other/welcome-calendar.js
+++ medias/Categorised/Dist/js/other/welcome-calendar.js
@@ -1,4 +1,4 @@
-!function(t){if(t){var e=stir.node("#welcomeeventfilters");const a=t=>(console.log(t),`<div class="grid-x u-bg-white u-mb-2 u-energy-line-left u-border-width-5">
+!function(t){if(!t)return;var e=stir.node("#welcomeeventfilters");const l=t,d=t=>`<div class="grid-x u-bg-white u-mb-2 u-energy-line-left u-border-width-5">
                   <div class="cell u-p-2 small-12  ">
                       
                       <p class="u-text-regular u-mb-2">
@@ -17,15 +17,23 @@
                               <span class="u-icon h5 uos-location"></span>
                               <span>${t.location}</span>
                           </div>
-                          
-                      <!-- div class="flex-container u-gap-16 align-middle">
-                              <span class="u-icon h5 uos-discounts"></span><a href="${t.link}">Booking required</a></ -->
+
+                          <div class="flex-container u-gap-16 align-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:20px; height: 20px; color: #006938 ">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                                </svg>
+                                <a href="${t.link}">More information</a>
+                            </div>
+
                       </div>
+
                       <p class="text-sm">${t.description}</p>
-                      <p class="u-m-0 text-sm">Theme: ${t.theme} <br />Attendance: ${t.attendance}</p>
+                      <p class="u-m-0 text-sm"><strong>Theme:</strong> ${t.theme} <br /><strong>Attendance:</strong> ${t.attendance}</p>
+
+
                   </div>
-              </div>`);var s=(t,e)=>`<select id="${e.toLowerCase().replaceAll(" ","-")}"><option value="">${e}</option>${t}</select>`;var i=stir.curry((t,e)=>(stir.setHTML(t,e),!0));const n=i(t);t=i(e);const l=stir.join(""),o=(t,e)=>t.startInt-e.startInt;i=stir.curry((e,s)=>{var i={},r=[];for(let t=0;t<s.length;t++)i[s[t][e]]||(i[s[t][e]]=!0,r.push(s[t]));return r});const c=stir.curry((t,e)=>!t||e.startInt===Number(t)),d=stir.curry((t,e)=>!t||e.theme===t),p=stir.feeds.events.filter(t=>t.id);console.log(p);var r=i("startInt"),r=stir.compose(l,stir.map(t=>`<option value="${t.startInt}">${t.stirStart}</option>`),r,stir.map(t=>({startInt:t.startInt,stirStart:t.stirStart})),stir.sort(o))(p),i=i("theme"),i=stir.compose(l,stir.map(t=>`<option value="${t.theme}">${t.theme}</option>`),i,stir.filter(t=>t.theme),stir.map(t=>({theme:t.theme})))(p);t(s(r,"Filter by date")+s(i,"Filter by theme"));const m=(t,e,s)=>{t=c(t),e=d(e),e=stir.compose(l,stir.map(a),stir.filter(e),stir.filter(t),stir.sort(o))(s);e.length?n(e):n(`<div class="grid-x u-bg-white u-mb-2  u-border-width-5">
+              </div>`;t=(t,e)=>`<select id="${e.toLowerCase().replaceAll(" ","-")}"><option value="">${e}</option>${t}</select>`;const c=stir.curry((t,e)=>(stir.setHTML(t,e),!0)),u=stir.curry((t,e)=>(t.insertAdjacentHTML("beforeend",e),t));var r=c(e);const m=stir.join(""),p=(t,e)=>t.startInt-e.startInt;var s=stir.curry((e,r)=>{var s={},i=[];for(let t=0;t<r.length;t++)s[r[t][e]]||(s[r[t][e]]=!0,i.push(r[t]));return i});const v=stir.curry((t,e)=>!t||e.startInt===Number(t)),g=stir.curry((t,e)=>!t||e.theme===t);const h=stir.filter(t=>t.endInt>=i()),f=stir.curry((t,e,r)=>{t=e*(t-1);return t<=r&&r<t+e}),i=()=>{var t=new Date;return Number(t.toISOString().split("T")[0].split("-").join(""))},n=(t,e,r)=>{const s=Number(QueryParams.get("page"))||1;var i=5*(s-1),n=5+i,a=(1===s?c:u)(l),o=stir.filter((t,e)=>{if(f(s,5,e))return t}),t=v(t),e=g(e),e=stir.compose(stir.filter(e),stir.filter(t),stir.sort(p),h)(r),t=stir.compose(m,stir.map(d),o)(e),r=e.length;t.length?a((o=n,e=r,((i=i)<2?"":`<div class="flex-container align-center u-mb-2">Showing ${i+1}-${e<o?e:o} of ${e} results</div>`)+t+(i=n,o=r,console.log(i,o),o<=i?"":'<div class="loadmorebtn flex-container align-center u-mb-2" ><button class="button hollow tiny">Load more results</button></div>'))):a(`<div class="grid-x u-bg-white u-mb-2  u-border-width-5">
                   <div class="cell u-p-2 small-12  ">
                       <p>No events have been found for the criteria selected</p>
                   </div>
-              </div>`)};m("","",p),e.addEventListener("click",t=>{var e;"OPTION"===t.target.nodeName&&(t=stir.node("#filter-by-date"),e=stir.node("#filter-by-theme"),m(t.options[t.selectedIndex].value,e.options[e.selectedIndex].value,p))})}}(stir.node("#welcomeevents"));
+              </div>`)},a=stir.feeds.events.filter(t=>t.id);QueryParams.set("page",1);var o=s("startInt"),o=stir.compose(m,stir.map(t=>`<option value="${t.startInt}">${t.stirStart}</option>`),o,stir.map(t=>({startInt:t.startInt,stirStart:t.stirStart})),stir.sort(p),h)(a),s=s("theme"),s=stir.compose(m,stir.map(t=>`<option value="${t.theme}">${t.theme}</option>`),s,stir.filter(t=>t.theme),stir.map(t=>({theme:t.theme})))(a);r(t(o,"Filter by date")+t(s,"Filter by theme")),n("","",a),l.addEventListener("click",t=>{var e,r,s;"submit"===t.target.type&&(e=stir.node("#filter-by-date"),r=stir.node("#filter-by-theme"),s=Number(QueryParams.get("page")),c(t.target.closest(".loadmorebtn"),""),QueryParams.set("page",s+1),n(e.options[e.selectedIndex].value,r.options[r.selectedIndex].value,a))}),e.addEventListener("click",t=>{var e;"OPTION"===t.target.nodeName&&(t=stir.node("#filter-by-date"),e=stir.node("#filter-by-theme"),n(t.options[t.selectedIndex].value,e.options[e.selectedIndex].value,a))})}(stir.node("#welcomeevents"));
--- src/js-other-2/welcome-calendar.js
+++ src/js-other-2/welcome-calendar.js
@@ -2,6 +2,9 @@
   if (!scope) return;
 
   const filtersArea = stir.node("#welcomeeventfilters");
+  const resultsArea = scope;
+
+  const ITEMS_PER_PAGE = 5;
 
   /*
       | 
@@ -10,7 +13,6 @@
       */
 
   const renderEvent = (item) => {
-    console.log(item);
     return `<div class="grid-x u-bg-white u-mb-2 u-energy-line-left u-border-width-5">
                   <div class="cell u-p-2 small-12  ">
                       
@@ -30,12 +32,20 @@
                               <span class="u-icon h5 uos-location"></span>
                               <span>${item.location}</span>
                           </div>
-                          
-                      <!-- div class="flex-container u-gap-16 align-middle">
-                              <span class="u-icon h5 uos-discounts"></span><a href="${item.link}">Booking required</a></ -->
+
+                          <div class="flex-container u-gap-16 align-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:20px; height: 20px; color: #006938 ">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                                </svg>
+                                <a href="${item.link}">More information</a>
+                            </div>
+
                       </div>
+
                       <p class="text-sm">${item.description}</p>
-                      <p class="u-m-0 text-sm">Theme: ${item.theme} <br />Attendance: ${item.attendance}</p>
+                      <p class="u-m-0 text-sm"><strong>Theme:</strong> ${item.theme} <br /><strong>Attendance:</strong> ${item.attendance}</p>
+
+
                   </div>
               </div>`;
   };
@@ -60,6 +70,15 @@
               </div>`;
   };
 
+  const renderPaginationBtn = (end, noOfResults) => {
+    console.log(end, noOfResults);
+    return end >= noOfResults ? `` : `<div class="loadmorebtn flex-container align-center u-mb-2" ><button class="button hollow tiny">Load more results</button></div>`;
+  };
+
+  const renderPageMeta = (start, end, noOfResults) => {
+    return start < 2 ? `` : `<div class="flex-container align-center u-mb-2">Showing ${start + 1}-${end > noOfResults ? noOfResults : end} of ${noOfResults} results</div>`;
+  };
+
   /*
       | 
       |  HELPERS
@@ -71,7 +90,10 @@
     return true;
   });
 
-  const setDOMEvents = setDOMContent(scope);
+  const appendDOMContent = stir.curry((elem, html) => {
+    elem.insertAdjacentHTML("beforeend", html);
+    return elem;
+  });
 
   const setDOMDateFilter = setDOMContent(filtersArea);
 
@@ -113,47 +135,89 @@
     return item.theme === theme;
   });
 
+  const isUpcoming = (item) => item.endInt >= getNow();
+
+  const isUpcomingFilter = stir.filter(isUpcoming);
+
+  const paginationFilter = stir.curry((page, itemsPerPage, index) => {
+    const start = itemsPerPage * (page - 1);
+    const end = start + itemsPerPage;
+    return index >= start && index < end;
+  });
+
+  const getNow = () => {
+    let yourDate = new Date();
+    return Number(yourDate.toISOString().split("T")[0].split("-").join(""));
+  };
+
+  /* 
+        CONTROLLERS
+    */
+  const doEventsFilter = (date, theme, data) => {
+    const page = Number(QueryParams.get("page")) || 1;
+    const start = ITEMS_PER_PAGE * (page - 1);
+    const end = start + ITEMS_PER_PAGE;
+
+    const setDOMEvents = page === 1 ? setDOMContent(resultsArea) : appendDOMContent(resultsArea);
+
+    const pageFilterCurry = stir.filter((item, index) => {
+      if (paginationFilter(page, ITEMS_PER_PAGE, index)) return item;
+    });
+
+    const filterByDateCurry = filterByDate(date);
+    const filterByThemeCurry = filterByTheme(theme);
+
+    const data2 = stir.compose(stir.filter(filterByThemeCurry), stir.filter(filterByDateCurry), stir.sort(sortByStartDate), isUpcomingFilter)(data);
+
+    const html = stir.compose(joiner, stir.map(renderEvent), pageFilterCurry)(data2);
+    const noOfResults = data2.length;
+
+    html.length ? setDOMEvents(renderPageMeta(start, end, noOfResults) + html + renderPaginationBtn(end, noOfResults)) : setDOMEvents(renderNoEvents());
+  };
+
   /*
-      | 
-      |  ON LOAD
-      |
-      */
+    | 
+    |  ON LOAD
+    |
+    */
 
   /* 
         Welcome Events 
      */
   const initData = stir.feeds.events.filter((item) => item.id);
 
-  console.log(initData);
+  QueryParams.set("page", 1);
 
   /* 
         Filters 
     */
   const removeDateDups = removeDuplicateObjectFromArray("startInt");
-  const datesHtml = stir.compose(joiner, stir.map(renderDateFilter), removeDateDups, stir.map(mapDates), stir.sort(sortByStartDate))(initData);
+  const datesHtml = stir.compose(joiner, stir.map(renderDateFilter), removeDateDups, stir.map(mapDates), stir.sort(sortByStartDate), isUpcomingFilter)(initData);
 
   const removeFilterDups = removeDuplicateObjectFromArray("theme");
   const themesHtml = stir.compose(joiner, stir.map(renderThemeFilter), removeFilterDups, stir.filter(filterThemeEmpties), stir.map(mapTheme))(initData);
 
   setDOMDateFilter(renderSelectFilter(datesHtml, "Filter by date") + renderSelectFilter(themesHtml, "Filter by theme"));
 
-  /* 
-        CONTROLLER 
-    */
-  const doEventsFilter = (date, theme, data) => {
-    const filterByDateCurry = filterByDate(date);
-    const filterByThemeCurry = filterByTheme(theme);
-
-    const html = stir.compose(joiner, stir.map(renderEvent), stir.filter(filterByThemeCurry), stir.filter(filterByDateCurry), stir.sort(sortByStartDate))(data);
-
-    html.length ? setDOMEvents(html) : setDOMEvents(renderNoEvents());
-  };
-
+  /* default list */
   doEventsFilter("", "", initData);
 
   /* 
         EVENT LISTENER 
     */
+
+  resultsArea.addEventListener("click", (event) => {
+    if (event.target.type === "submit") {
+      const dateFilter = stir.node("#filter-by-date");
+      const themeFilter = stir.node("#filter-by-theme");
+      const page = Number(QueryParams.get("page"));
+
+      setDOMContent(event.target.closest(".loadmorebtn"), "");
+      QueryParams.set("page", page + 1);
+      doEventsFilter(dateFilter.options[dateFilter.selectedIndex].value, themeFilter.options[themeFilter.selectedIndex].value, initData);
+    }
+  });
+
   filtersArea.addEventListener("click", (event) => {
     if (event.target.nodeName === "OPTION") {
       const dateFilter = stir.node("#filter-by-date");
