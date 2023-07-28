4e722777a14a2978cf574efaf779c318524e470e
Ryan Kaye <ryankaye@gmail.com>
2023-07-28 11:30:26 +0100

    Auto stash before merge of "main" and "origin/main"

--- medias/Categorised/Dist/js/other/_unminified/welcome-calendar.js
+++ medias/Categorised/Dist/js/other/_unminified/welcome-calendar.js
@@ -6,13 +6,23 @@
 
   const ITEMS_PER_PAGE = 9;
 
-  console.log("hi");
-
   /*
       | 
       |  RENDERERS
       |
       */
+
+  const renderLink = (link) => {
+    return !link
+      ? ``
+      : `
+      <div class="flex-container u-gap-16 align-middle">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:20px; height: 20px; color: #006938 ">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+          </svg>
+          <a href="${link}">More information</a>
+        </div>`;
+  };
 
   const renderEvent = (item) => {
     return `<div class="grid-x u-bg-white u-mb-2 u-energy-line-left u-border-width-5">
@@ -35,12 +45,7 @@
                               <span>${item.location}</span>
                           </div>
 
-                          <div class="flex-container u-gap-16 align-middle">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="width:20px; height: 20px; color: #006938 ">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                                </svg>
-                                <a href="${item.link}">More information</a>
-                            </div>
+                          ${renderLink(item.link)}
 
                       </div>
 
