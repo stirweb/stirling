52e06372e26f6363aecf66bc9b7368f1ad4f9b55
Ryan Kaye <ryankaye@gmail.com>
2023-08-30 11:56:17 +0100

    Course start date listing: Wee tidy

--- medias/Categorised/Dist/js/other/course-startdate-list.js
+++ medias/Categorised/Dist/js/other/course-startdate-list.js
@@ -1,12 +1,12 @@
-!function(t){if(!t)return;const r={applyLink:"https://portal.stir.ac.uk/student/course-application/ugd/application.jsp?crsCode=",month:t.getAttribute("data-startmonth")};Object.freeze(r);stir.curry((r,s)=>{const i=t=>t.includes("UDX12")?" Apply for BA (Hons)":t.includes("UDX16")?" Apply for BSc (Hons)":"Apply";return s.portalapply&&""!==s.portalapply?s.portalapply.split(",").map(t=>'<a aria-label="'+i(t)+" "+s.title+'" href="'+r.applyLink+t.trim()+'">'+i(t)+"</a>").join(" / "):""});const i=stir.curry((t,r)=>`
+!function(t){if(t){const o={applyLinkUG:"https://portal.stir.ac.uk/student/course-application/ugd/application.jsp?crsCode=",month:t.getAttribute("data-startmonth")},n=(Object.freeze(o),stir.curry((t,r)=>{return`
         <tr>
             <td>
               ${r.url?`<a href="${r.url}">`:""}
               ${r.prefix} ${r.title} 
               ${r.url?"</a>":""}
             </td>
-            <td>${e(r,t.month)}</td>
-        </tr>`);var s=stir.curry((t,r)=>{const s=i(t);return`
+            <td>${r=r,s=t.month,r.starts?stir.compose(stir.join(", "),stir.map(t=>t.split(" ")[1]),stir.filter(t=>t.includes(s)))(r.starts.split(", ")):""}</td>
+        </tr>`;var s}));var r,s,i=stir.curry((t,r)=>{const s=n(t);return`
           <table>
               <caption>Courses starting in ${t.month}</caption>
               <thead>
@@ -15,4 +15,4 @@
               <tbody>
                   ${r.map(t=>s(t)).join("")}
               </tbody>
-          </table>`});const e=(t,r)=>t.starts?stir.compose(stir.join(", "),stir.map(t=>t.split(" ")[1]),stir.filter(t=>t.includes(r)))(t.starts.split(", ")):"";var a,l,o=stir.curry((t,r)=>(t.innerHTML=r,t)),p=stir.feeds.data||[];p.length&&(a=stir.filter(t=>t.starts&&t.starts.includes(r.month)),stir.filter(t=>t.portalapply),l=stir.sort((t,r)=>t.title<r.title?-1:t.title>r.title?1:0),o=o(t),t=s(r),stir.compose(o,t,l,l,a,stir.clone)(p))}(stir.node("#course-list"));
+          </table>`}),e=stir.curry((t,r)=>(t.innerHTML=r,t)),a=stir.feeds.data||[];a.length&&(r=stir.filter(t=>t.starts&&t.starts.includes(o.month)),s=stir.sort((t,r)=>t.title<r.title?-1:t.title>r.title?1:0),e=e(t),t=i(o),stir.compose(e,t,s,s,r,stir.clone)(a))}}(stir.node("#course-list"));
