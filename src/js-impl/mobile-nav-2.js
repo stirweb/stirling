2c6d7927134129d6fd022bce60a0c8708ba3ea64
Ryan Kaye <ryankaye@gmail.com>
2023-09-29 23:21:53 +0100

    App.js: Refactor into single global function (stir.lazyJS))

--- src/js-impl/mobile-nav-2.js
+++ src/js-impl/mobile-nav-2.js
@@ -1,21 +1,24 @@
 (function () {
-  const scriptSrc = UoS_env.name.includes("preview") ? `<t4 type="media" id="174054" formatter="path/*" />` : UoS_env.wc_path + "js/other/" + "mobile-nav.js";
+  //const scriptSrc = UoS_env.name.includes("preview") ? `<t4 type="media" id="174054" formatter="path/*" />` : UoS_env.wc_path + "js/other/" + "mobile-nav.js";
 
-  const nodes = ["#open_mobile_menu"];
-  const nodesInUse = nodes.filter((item) => stir.node(item));
+  //const nodes = ["#open_mobile_menu"];
 
-  if (!nodesInUse.length) return;
+  stir.lazyJS(["#open_mobile_menu"], "mobile-nav.js", "174054");
 
-  nodesInUse.forEach((item) => {
-    let observer = stir.createIntersectionObserver({
-      element: stir.node(item),
-      threshold: [0.001],
-      callback: function (entry) {
-        if (entry.isIntersecting) {
-          stir.addScript(scriptSrc);
-          observer && observer.observer.unobserve(this);
-        }
-      },
-    });
-  });
+  // const nodesInUse = nodes.filter((item) => stir.node(item));
+
+  // if (!nodesInUse.length) return;
+
+  // nodesInUse.forEach((item) => {
+  //   let observer = stir.createIntersectionObserver({
+  //     element: stir.node(item),
+  //     threshold: [0.001],
+  //     callback: function (entry) {
+  //       if (entry.isIntersecting) {
+  //         stir.addScript(scriptSrc);
+  //         observer && observer.observer.unobserve(this);
+  //       }
+  //     },
+  //   });
+  // });
 })();
--- src/js-impl/stir-tabs.js
+++ src/js-impl/stir-tabs.js
@@ -1,21 +1,3 @@
 (function () {
-  const scriptSrc = UoS_env.name.includes("preview") ? `<t4 type="media" id="174053" formatter="path/*" />` : UoS_env.wc_path + "js/other/" + "stir-tabs.js";
-
-  const nodes = ['[data-behaviour="tabs"]'];
-  const nodesInUse = nodes.filter((item) => stir.node(item));
-
-  if (!nodesInUse.length) return;
-
-  nodesInUse.forEach((item) => {
-    let observer = stir.createIntersectionObserver({
-      element: stir.node(item),
-      threshold: [0.001],
-      callback: function (entry) {
-        if (entry.isIntersecting) {
-          stir.addScript(scriptSrc);
-          observer && observer.observer.unobserve(this);
-        }
-      },
-    });
-  });
+  stir.lazyJS(['[data-behaviour="tabs"]'], "stir-tabs.js", "174053");
 })();
