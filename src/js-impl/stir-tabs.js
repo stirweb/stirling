7612ef9e0b86f3bfa76ceb1494b3cba4d2258434
Ryan Kaye <ryankaye@gmail.com>
2023-09-29 21:36:20 +0100

    App.js: Test lazy load tabs js - amend preview id

--- src/js-impl/stir-tabs.js
+++ src/js-impl/stir-tabs.js
@@ -3,7 +3,7 @@
    Set up any tabs found on the page
  */
 (function () {
-  const scriptSrc = UoS_env.name.includes("preview") ? `<t4 type="media" id="174052" formatter="path/*" />` : UoS_env.wc_path + "js/other/" + "stir-tabs.js";
+  const scriptSrc = UoS_env.name.includes("preview") ? `<t4 type="media" id="174053" formatter="path/*" />` : UoS_env.wc_path + "js/other/" + "stir-tabs.js";
 
   const nodes = ['[data-behaviour="tabs"]'];
   const nodesInUse = nodes.filter((item) => stir.node(item));
