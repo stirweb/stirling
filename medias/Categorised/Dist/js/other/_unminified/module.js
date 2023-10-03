0b58d9d3e2236fa153b2647c364d02416cb14ab3
Ryan Kaye <ryankaye@gmail.com>
2023-10-03 14:07:38 +0100

    Modules JS: New API Integrations

--- medias/Categorised/Dist/js/other/_unminified/module.js
+++ medias/Categorised/Dist/js/other/_unminified/module.js
@@ -119,12 +119,6 @@
       AND POPULATE THE PAGE
   
   */
-  const url = "https://www.stir.ac.uk/data/courses/akari/module/index.php?module=";
-
-  const colours = [
-    { level: "ug", first: "heritage-green", second: "energy-turq", third: "energy-purple" },
-    { level: "pg", first: "heritage-purple", second: "heritage-purple", third: "heritage-green" },
-  ];
 
   /* 
 
@@ -227,9 +221,11 @@
   };
 
   const renderDisclaimer = () => {
-    return `<div class="cell bg-grey u-bleed u-p-2 "><p class="u-m-0 text-md">We aim to present detailed, up-to-date module information - in fact, we're providing more 
-            information than ever. However, modules and courses are constantly being enhanced to boost your learning experience, and are therefore subject 
-            to change. <a href="#">See terms and conditions</a>.</p></div>`;
+    return `<div class="cell bg-grey u-bleed u-p-2 ">
+              <p class="u-m-0 text-md">We aim to present detailed, up-to-date module information - in fact, we're providing more 
+              information than ever. However, modules and courses are constantly being enhanced to boost your learning experience, and are therefore subject 
+              to change. <a href="#">See terms and conditions</a>.</p>
+          </div>`;
   };
 
   const renderContentAims = ({ moduleOverview, learningOutcomes, colourPack }) => {
@@ -254,10 +250,7 @@
   };
 
   const renderAccreditation = (professionalAccreditation, colour) => {
-    return !professionalAccreditation
-      ? ``
-      : `<h3 class="header-stripped u-bg-${colour}--10 u-p-1 u-${colour}-line-left u-border-width-5 u-text-regular">Professional accreditation</h3>
-          <p>${professionalAccreditation}</p>`;
+    return !professionalAccreditation ? `` : `<h3 class="header-stripped u-bg-${colour}--10 u-p-1 u-${colour}-line-left u-border-width-5 u-text-regular">Professional accreditation</h3><p>${professionalAccreditation}</p>`;
   };
 
   const renderAwards = ({ modulecredits, ectsmodulecredits, professionalAccreditation, colourPack }) => {
@@ -290,71 +283,51 @@
     return `<div class="cell u-mt-2">
                 <h2 id="requirements" class="u-scroll-offset">Study requirements</h2>
                 ${renderPrerequisites(modulerequisites)}
-
                 <p>Co-requisites: This module must be studied in conjunction with: module name (code)</p>
             </div>`;
   };
 
   const renderStudyAbroad = () => {
-    return `<p>This module is available to suitably-qualified undergraduate students studying elsewhere in the
-                        world who wish to join Stirling for a semester or academic year. <a href="">Learn more</a></p>`;
+    return `<p>This module is available to suitably-qualified students studying elsewhere in the world who wish to join Stirling for a semester or academic year. <a href="">Learn more</a></p>`;
   };
 
   const renderSupportingInfo = (preparedotherinformation) => {
-    return `<h3 class="header-stripped u-bg-heritage-green--10 u-p-1 u-heritage-line-left u-border-width-5 u-text-regular">Supporting notes</h3>
-            <p>${preparedotherinformation}</p>`;
+    return `<h3 class="header-stripped u-bg-heritage-green--10 u-p-1 u-heritage-line-left u-border-width-5 u-text-regular">Supporting notes</h3><p>${preparedotherinformation}</p>`;
   };
 
   const renderFurtherDetails = (data) => {
     return `<div class="cell u-mt-2">
-                    <h2 id="further" class="u-scroll-offset">Further details</h2>
-                    
-                    ${data.preparedotherinformation ? renderSupportingInfo(data.preparedotherinformation) : ``}
-                   
-                    <h3 class="header-stripped u-bg-heritage-green--10 u-p-1 u-heritage-line-left u-border-width-5 u-text-regular">Visiting overseas students</h3>
-                    ${data.studyAbroad === "Yes" ? renderStudyAbroad() : `<p>Not available</p>`}
-                    
-                    <h3 class="header-stripped u-bg-heritage-green--10 u-p-1 u-heritage-line-left u-border-width-5 u-text-regular">Additional costs</h3>
-                    <p>${data.additionalCosts}</p>
-                </div>`;
-  };
-
-  const renderDeliveries = (deliveries, width, colourPack) => {
-    return !deliveries
-      ? ``
-      : `<div class="cell large-${width} u-mb-1">
-            <h3 class="header-stripped u-bg-${colourPack.second}--10 u-p-1 u-${colourPack.second}-line-left u-border-width-5 u-text-regular">Engagement overview</h3>
-            ${deliveries}
-        </div>`;
-  };
-
-  const renderAssessment = (assessments, width, colourPack) => {
-    return !assessments
-      ? ``
-      : `<div class="cell large-${width} u-mb-1">
-            <h3 class="header-stripped u-bg-${colourPack.second}--10 u-p-1 u-${colourPack.second}-line-left u-border-width-5 u-text-regular">Assessment overview</h3>
-            ${assessments}
-        </div>`;
+                <h2 id="further" class="u-scroll-offset">Further details</h2>
+                ${data.preparedotherinformation ? renderSupportingInfo(data.preparedotherinformation) : ``}
+                <h3 class="header-stripped u-bg-heritage-green--10 u-p-1 u-heritage-line-left u-border-width-5 u-text-regular">Visiting overseas students</h3>
+                ${data.studyAbroad === "Yes" ? renderStudyAbroad() : `<p>Not available</p>`}
+                <h3 class="header-stripped u-bg-heritage-green--10 u-p-1 u-heritage-line-left u-border-width-5 u-text-regular">Additional costs</h3>
+                <p>${data.additionalCosts}</p>
+            </div>`;
   };
 
   const renderDeliverablesTotal = (hours, colourPack) => {
-    return `<div class="u-bg-${colourPack.second}--10 u-p-tiny u-p-1 u-text-regular u-mt-1 flex-container ">
+    return `<div class="u-bg-${colourPack.second}--10 u-p-tiny u-p-1 u-text-regular u-mt-1 flex-container u-mb-2">
                 <strong class="u-flex1">Total workload</strong>
                 <strong>${hours} hours</strong>
             </div>`;
   };
 
-  const renderDeliverables = stir.curry((colourPack, total, { label, hours, typekey }) => {
+  const renderDeliverables = stir.curry((colourPack, total, { type, hours, typekey, label }) => {
     return typekey === "total"
       ? renderDeliverablesTotal(hours, colourPack)
       : `
         <div>
-            <span class="u-inline-block u-p-tiny u-px-1">${label}</span>
+            <span class="u-inline-block u-p-tiny u-px-1">${type}</span>
             <div class="barchart u-relative u-flex align-middle u-overflow-hidden u-bg-grey--mid" data-value="${hours}" data-max="${total}" data-unit="" data-colour="${colourPack.second}"></div>
         </div>`;
   });
 
-  const renderAssessments = stir.curry((colourPack, { label, category, percent }) => {
+  const renderDeliveries = (width, deliveries) => (!deliveries ? `` : `<div class="cell large-${width} u-mb-1">${deliveries}</div>`);
+
+  const renderDeliveriesFallback = () => `<p class="u-m-0">Engagement and teaching information isn't currently available, but it will be made clear to you when you make your module selections.</p>`;
+
+  const renderAssessmentItem = stir.curry((colourPack, { label, category, percent }) => {
     return percent === "0"
       ? ``
       : `
@@ -364,22 +337,37 @@
         </div>`;
   });
 
-  const renderTeachingAssessment = (deliveries, assessments, colourPack) => {
-    const width = !deliveries || !assessments ? `12` : `6`;
+  const renderAssessments = stir.curry((colourPack, { tab, tabAssessments }) => {
+    const renderAssessmentItemCurry = renderAssessmentItem(colourPack);
+    return `<h4>${tab}</h4><p>${tabAssessments.map(renderAssessmentItemCurry).join(``)}</p>`;
+  });
 
-    return !deliveries && !assessments
-      ? ``
-      : `<div class="cell u-mt-2">
+  const renderAssessment = stir.curry((width, item) => (!item ? `` : `<div class="cell large-${width} u-mb-1">${item}</div>`));
+
+  const renderAssessmentFallback = () => `Engagament information isn't currently available to be displayed for this module. You'll be able to find out more when enrolling for your modules.`;
+
+  const renderTeachingAssessment = (deliveries, assessments, colourPack) => {
+    const deliveriesHtml = !deliveries.length ? renderDeliveriesFallback() : renderDeliveries(`12`, deliveries);
+
+    const assessmentWidth = assessments.length < 2 ? `12` : `6`;
+    const renderAssessmentCurry = renderAssessment(assessmentWidth);
+    const assessmentHtml = !assessments.length ? renderAssessmentFallback() : assessments.map(renderAssessmentCurry).join(``);
+
+    return `<div class="cell u-mt-2">
               <h2 id="teaching" class="u-scroll-offset">Teaching and assessment</h2>
 
               <p>Here's an overview of the learning, teaching and assessment methods, and the recommended time you
                   should dedicate to the study of this module. Most modules include a combination of activity
                   (e.g. lectures), assessments and self-study.</p>
+              
+              <h3 class="header-stripped u-bg-${colourPack.second}--10 u-p-1 u-${colourPack.second}-line-left u-border-width-5 u-text-regular u-mt-2">Engagement overview</h3>
+              ${deliveriesHtml}
 
-              <div class="grid-x grid-padding-x u-my-2">
-                  ${renderDeliveries(deliveries, width, colourPack)}
-                  ${renderAssessment(assessments, width, colourPack)}
+              <h3 class="header-stripped u-bg-${colourPack.second}--10 u-p-1 u-${colourPack.second}-line-left u-border-width-5 u-text-regular u-m-0 u-mt-3 u-mb-1">Assessment overview</h3>
+              <div class="grid-x grid-padding-x ">
+                  ${assessmentHtml}
               </div>
+
               <p>Are you an incoming Stirling student? You'll typically receive timetables for module-level
                   lectures one month prior
                   - and select seminars two weeks prior - to the start of your first semester. Help with module
@@ -393,21 +381,21 @@
 
   const renderError = () => renderSectionStart() + `<div class="cell u-padding-y"><h1>Page not found</h1></div>` + renderSectionEnd();
 
-  const renderDebugDataItem = (item) => {
-    if (item.category) {
-      return `<div class="flex-container u-border-bottom-solid u-p-tiny"><span class="u-flex1">${item.label} (${item.category})</span><span>${item.percent}%</span></div>`;
-    }
-    return `<div class="flex-container u-border-bottom-solid u-p-tiny"><span class="u-flex1">${item.label} (hours)</span><span>${item.hours}</span></div>`;
-  };
+  // const renderDebugDataItem = (item) => {
+  //   if (item.category) {
+  //     return `<div class="flex-container u-border-bottom-solid u-p-tiny"><span class="u-flex1">${item.label} (${item.category})</span><span>${item.percent}%</span></div>`;
+  //   }
+  //   return `<div class="flex-container u-border-bottom-solid u-p-tiny"><span class="u-flex1">${item.label} (hours)</span><span>${item.hours}</span></div>`;
+  // };
 
-  const renderDebug = (total, sum, unit, data) => {
-    return `<div class="u-border-solid u-p-1" style="color:#d51212">
-              <p><strong>Error with the data</strong></p>
-              <p>Reported total: ${total} ${unit}<br>
-              Actual sum: ${sum} </p>
-              ${data.map(renderDebugDataItem).join(``)} 
-            </div>`;
-  };
+  // const renderDebug = (total, sum, unit, data) => {
+  //   return `<div class="u-border-solid u-p-1" style="color:#d51212">
+  //             <p><strong>Error with the data</strong></p>
+  //             <p>Reported total: ${total} ${unit}<br>
+  //             Actual sum: ${sum} </p>
+  //             ${data.map(renderDebugDataItem).join(``)}
+  //           </div>`;
+  // };
 
   /*
         INPUT / OUTPUT EVENTS (SIDE EFFECTS!!)
@@ -456,7 +444,7 @@
     const deliveriesTotalValue = deliveriesTotalItem.length ? deliveriesTotalItem[0].hours : null;
 
     const renderDeliverablesCurry = renderDeliverables(colourPack, deliveriesTotalValue);
-    const deliveriesTotalFiltered = deliveries.filter((item) => item.typekey !== "total");
+    //const deliveriesTotalFiltered = deliveries.filter((item) => item.typekey !== "total");
 
     const total = Number(deliveriesTotalValue);
     const sum = deliveries
@@ -466,25 +454,32 @@
         return accumulator + currentValue;
       }, 0);
 
-    return Number(total) !== sum ? renderDebug(total, sum, `Hours (Total Study Time)`, deliveriesTotalFiltered) : deliveries.map(renderDeliverablesCurry).join(``);
+    return Number(total) !== sum ? `` : deliveries.map(renderDeliverablesCurry).join(``);
+    //return Number(total) !== sum ? renderDebug(total, sum, `Hours (Total Study Time)`, deliveriesTotalFiltered) : deliveries.map(renderDeliverablesCurry).join(``);
   };
 
-  // Assessments
-  const doAssessments = (assessments, colourPack) => {
-    // const mapped = assessments.map((item) => {
-    //   return { ...item, match: item.label + item.category + item.percent };
-    // });
-    const renderAssessmentsCurry = renderAssessments(colourPack);
-
-    // const filterDups = removeDuplicateObjectFromArray("match", mapped);
-    const totalPercent = 100;
-    const sum = assessments
+  const doAssessmentItem = (item) => {
+    const sum = item.tabAssessments
       .map((item) => Number(item.percent))
       .reduce((accumulator, currentValue) => {
         return accumulator + currentValue;
       }, 0);
 
-    return totalPercent !== sum ? renderDebug(totalPercent, sum, "(Percent)", assessments) : assessments.map(renderAssessmentsCurry).join(``);
+    return { sum: sum, assessment: item };
+  };
+
+  // doAssessments
+  const doAssessments = (assessments, colourPack) => {
+    const renderAssessmentsCurry = renderAssessments(colourPack);
+    // const filterDups = removeDuplicateObjectFromArray("match", mapped);
+    const totalPercent = 100;
+    const sums = assessments.map(doAssessmentItem);
+
+    return sums.map((item) => {
+      if (item.sum !== totalPercent) return ``;
+      return renderAssessmentsCurry(item.assessment);
+    });
+    //return totalPercent !== sum ? renderDebug(totalPercent, sum, "(Percent)", assessments) : assessments[0].tabAssessments.map(renderAssessmentsCurry).join(``);
   };
 
   // Main
@@ -497,7 +492,7 @@
     const colourPack = getColourPack(studyLevel, colours);
     const data2 = { ...data, colourPack: colourPack };
 
-    const deliveries = doDeliveries(data.deliveries, colourPack);
+    const deliveries = ""; //doDeliveries(data.deliveries, colourPack);
     const assessments = doAssessments(data.assessments, colourPack);
 
     const html = renderHeader(data2) + renderStickyNav(studyLevel) + renderSectionStart() + renderDisclaimer() + renderContentAims(data2) + renderTeachingAssessment(deliveries, assessments, colourPack) + renderAwards(data2) + renderStudyRequirements(data2) + renderFurtherDetails(data2) + renderSectionEnd();
@@ -512,12 +507,11 @@
 
     try {
       const data = await response.json();
-
-      console.log(data);
       main(data, colours);
       addEventListeners();
     } catch (error) {
-      console.log(error.message);
+      setDOMContent(contentArea, renderError());
+      //console.log(error.message);
     }
   }
 
@@ -525,9 +519,15 @@
         On Load
     */
 
+  const url = "https://www.stir.ac.uk/data/courses/akari/module/index.php?module=";
+
+  const colours = [
+    { level: "ug", first: "heritage-green", second: "energy-turq", third: "energy-purple" },
+    { level: "pg", first: "heritage-purple", second: "heritage-purple", third: "heritage-green" },
+  ];
+
   const params = new URLSearchParams(document.location.search);
   const fetchUrl = url + [params.get("code"), params.get("session"), params.get("semester")].join("/");
 
-  console.log(fetchUrl);
   getData(fetchUrl, colours);
 })();
