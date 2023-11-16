2f9d7754904dfa5b312c60ca10e9c95457a23196
Ryan Kaye <ryankaye@gmail.com>
2023-11-16 12:07:35 +0000

    Module full stops. Rewrite half and half code

--- medias/Categorised/Dist/js/other/_unminified/module.js
+++ medias/Categorised/Dist/js/other/_unminified/module.js
@@ -115,10 +115,10 @@
                 <h2 id="awards">Awards</h2>
                 <h3 class="header-stripped u-bg-${colourPack.third}--10 u-p-1 u-${colourPack.third}-line-left u-border-width-5 u-text-regular">Credits</h3>
                 <p class="flex-container u-gap align-middle"><img src="<t4 type="media" id="173616" formatter="path/*"/>" width="65" height="44" alt="Scotland flag" />
-                    This module is worth ${moduleCredits} SCQF (Scottish Credit and Qualifications Framework) credits</p>
+                    This module is worth ${moduleCredits} SCQF (Scottish Credit and Qualifications Framework) credits.</p>
 
                 <p class="flex-container u-gap align-middle"><img src="<t4 type="media" id="173615" formatter="path/*"/>" width="65" height="44" alt="EU flag" /> 
-                    This equates to ${ectsModuleCredits} ECTS (The European Credit Transfer and Accumulation System) credits</p>
+                    This equates to ${ectsModuleCredits} ECTS (The European Credit Transfer and Accumulation System) credits.</p>
 
                 <div class="u-mb-2 u-bg-${colourPack.third}--10 flex-container align-stretch ">
                     <span class="u-bg-${colourPack.third} u-white flex-container align-middle u-width-64 u-px-1 ">
@@ -388,6 +388,7 @@
   const params = new URLSearchParams(document.location.search);
   const fetchUrl = url + [params.get("code"), params.get("session"), params.get("semester")].join("/");
 
+  console.log(fetchUrl);
   //const fetchUrl = "sample.json"; // Testing
 
   getData(fetchUrl, colours, boilerplates);
