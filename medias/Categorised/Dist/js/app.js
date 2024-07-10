2b3bd14dd3e6cff6791d96d38375c47d95330c6d
Robert Morrison <r.w.morrison@stir.ac.uk>
2024-07-10 23:14:51 +0100

    update for QueryParams urlencoding issue

--- medias/Categorised/Dist/js/app.js
+++ medias/Categorised/Dist/js/app.js
@@ -1102,8 +1102,20 @@ var QueryParams = (function () {
   function _remove(name, reload, url, replace, noencode) {
     if (!url) url = window.location.href;
 
-	var newUrl = _removeURLParameter(url, noencode? name : encodeURIComponent(name));
-    if(newUrl!==url) replace ? _replaceStateHandler(newUrl): _pushStateHandler(newUrl);
+	/* old code here */
+	// var newUrl = _removeURLParameter(url, noencode? name : encodeURIComponent(name));
+	/* old code end */
+	
+	/* new code here */
+	var newUrl = url;
+	if(url.indexOf(name)>-1) {
+		newUrl = _removeURLParameter(url, name);
+	} else if (url.indexOf(encodeURIComponent(name))>-1) {
+		newUrl = _removeURLParameter(url, encodeURIComponent(name));
+	}
+	/* new code end */
+
+	if(newUrl!==url) replace ? _replaceStateHandler(newUrl): _pushStateHandler(newUrl);
 
     if (reload) {
       window.location.href = document.location.href;
@@ -3477,7 +3489,7 @@ stir.lazy(document.querySelectorAll('.stirlazy,[data-lazy-container]'));
       break;
 
     default: // live
-      url = "https://www.stir.ac.uk/developer-components/includes/template-external/mega-menu/";
+      url = "/developer-components/includes/template-external/mega-menu/";
       break;
   }
 
