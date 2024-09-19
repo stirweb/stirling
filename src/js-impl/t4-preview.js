9c7651ccce41917226fc0f553937e095d67e7bdb
Robert Morrison <r.w.morrison@stir.ac.uk>
2024-09-19 11:09:00 +0100

    enable preview tools in t4test

--- src/js-impl/t4-preview.js
+++ src/js-impl/t4-preview.js
@@ -4,6 +4,8 @@ var stir = stir || {};
   // if we are in preview, dynamically load the preview tools
   // otherwise just skip this
 
+  if("www.stir.ac.uk"===window.location.hostname) return;
+
   switch (window.location.hostname) {
     case "localhost":
       stir.addScript("/src/js-other/t4-preview-tools.js");
@@ -12,6 +14,7 @@ var stir = stir || {};
       stir.addScript("/stirling/src/js-other/qa-protect.js");
       break;
     case "stiracuk-cms01-production.terminalfour.net":
+    case "stiracuk-cms01-test.terminalfour.net":
       stir.addScript('<t4 type="media" id="158095" formatter="path/*" />');
       break;
   }
