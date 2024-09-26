b16f7141e44522ddc224f253bfc771ce0034b5a6
Ryan Kaye <ryankaye@gmail.com>
2024-09-26 17:32:39 +0100

    Events news by tag: news added

--- medias/Categorised/Dist/js/other/events-news-by-tags.js
+++ medias/Categorised/Dist/js/other/events-news-by-tags.js
@@ -1,7 +1,25 @@
-!function(){const e=t=>{return"string"==typeof t&&t.trim().length?`${(t=t.split(" "))[0]} ${t[1].slice(0,3)} `+t[2]:""},s=t=>`
-        <img class="show-for-medium" src="${t.image}" alt="Image for event: ${t.title}" loading="lazy" />
-        <time class="u-block u-my-1 u-grey--dark">${e(t.stirStart)} - ${e(t.stirEnd)}</time>
-        <h3 class="header-stripped u-mb-1 u-font-normal u-compress-line-height">
-            <a href="${t.url}" class="c-link u-inline">${t.title}</a>
-        </h3>
-        <p class="text-sm">${t.summary}</p>`,n=()=>Number((new Date).toISOString().split(".")[0].replaceAll(/[-:T]/g,"").slice(0,-2)),a=(t,e)=>parseInt(t.startInt)-parseInt(e.startInt),o=i=>t=>{return{...t,isupcoming:(e=i,(t=>Number(t.endInt)>e)(t))};var e},l=t=>t.id,c=t=>t.isupcoming,m=e=>t=>t.tags.includes(e),p=t=>t.filter((t,e)=>0===e);var t,i;{const u=stir.node("#eventListing"),d=u.dataset.tags;u&&d&&(t=UoS_env.name,i={dev:"../index.json",preview:'<t4 type="navigation" id="5214" />',default:'<t4 type="navigation" id="5214" />index.json'},fetch(i[t]||i.default).then(t=>t.ok?t.json():Promise.reject("Network response was not ok")).catch(t=>(console.error("There was a problem fetching the data:",t),[])).then(t=>{var e,i,r;t=t,e=d,i=u,r=n(),e=stir.compose(stir.map(s),p,stir.sort(a),stir.filter(m(e)),stir.filter(c),stir.map(o(r)),stir.filter(l))(t),i.innerHTML=e.join("")}))}}();
+!function(){const s=e=>{return"string"==typeof e&&e.trim().length?`${(e=e.split(" "))[0]} ${e[1].slice(0,3)} `+e[2]:""},a=(e,s)=>` <div class="cell small-12 medium-${e}">
+                <div class="grid-x">
+                    <div class="cell"><h2 class="header-stripped u-header--margin-stripped">News</h2></div>
+                    ${s}
+                </div>
+            </div>`,n=e=>` <div class="cell small-12 medium-4">
+                <div class="grid-x">
+                    <div class="cell"><h2 class="header-stripped u-header--margin-stripped">Events</h2></div>
+                    ${e}
+                </div>
+            </div>`,c=e=>`<div class="cell small-12 ">
+                <img class="show-for-medium" src="${e.image}" alt="Image for event: ${e.title}" loading="lazy" />
+                <time class="u-block u-my-1 u-grey--dark">${s(e.stirStart)} - ${s(e.stirEnd)}</time>
+                <h3 class="header-stripped u-mb-1 u-font-normal u-compress-line-height">
+                    <a href="${e.url}" class="c-link u-inline">${e.title}</a>
+                </h3>
+                <p class="text-sm">${e.summary}</p>
+            </div>`,d=stir.curry((e,s)=>`<div class="cell small-12 medium-${e}">
+                <img class="show-for-medium" src="${s.thumbnail}" alt="Image for article: ${s.title}" loading="lazy" />
+                <time class="u-block u-my-1 u-grey--dark">${s.date}</time>
+                <h3 class="header-stripped u-mb-1 u-font-normal u-compress-line-height">
+                    <a href="${s.url}" class="c-link u-inline">${s.title}</a>
+                </h3>
+                <p class="text-sm">${s.summary}</p>
+            </div>`),m=()=>Number((new Date).toISOString().split(".")[0].replaceAll(/[-:T]/g,"").slice(0,-2)),o=(e,s)=>parseInt(e.startInt)-parseInt(s.startInt),u=i=>e=>{return{...e,isupcoming:(s=i,(e=>Number(e.endInt)>s)(e))};var s},p=e=>e.id,h=e=>e.isupcoming,g=s=>e=>e.tags.includes(s),v=e=>e.filter((e,s)=>0===s);var e,i;{console.log(newsList);const $=stir.node("#newsEventListing"),f=$.dataset.tags;$&&f&&(e=UoS_env.name,i={dev:"../index.json",preview:'<t4 type="navigation" id="5214" />',default:'<t4 type="navigation" id="5214" />index.json'},fetch(i[e]||i.default).then(e=>e.ok?e.json():Promise.reject("Network response was not ok")).catch(e=>(console.error("There was a problem fetching the data:",e),[])).then(e=>{var s,i,t,l,r;e=e,s=newsList,i=f,t=$,l=m(),i=stir.compose(stir.map(c),v,stir.sort(o),stir.filter(g(i)),stir.filter(h),stir.map(u(l)),stir.filter(p))(e),l=1===i.length?2:3,e=2==l?6:4,r=2==l?8:12,e=d(e),e=stir.map(e,s).slice(0,l),t.innerHTML=a(r,e.join(""))+n(i.join(""))}))}}();
--- src/js-other-2/events-news-by-tags.js
+++ src/js-other-2/events-news-by-tags.js
@@ -1,21 +1,51 @@
 (function () {
   /* Renderers */
 
-  const renderDate = (date) => {
+  const renderEventDate = (date) => {
     if (typeof date !== "string" || !date.trim().length) return ``;
     const parts = date.split(" ");
     return `${parts[0]} ${parts[1].slice(0, 3)} ${parts[2]}`;
   };
 
-  const renderEvent = (item) => {
-    return `
-        <img class="show-for-medium" src="${item.image}" alt="Image for event: ${item.title}" loading="lazy" />
-        <time class="u-block u-my-1 u-grey--dark">${renderDate(item.stirStart)} - ${renderDate(item.stirEnd)}</time>
-        <h3 class="header-stripped u-mb-1 u-font-normal u-compress-line-height">
-            <a href="${item.url}" class="c-link u-inline">${item.title}</a>
-        </h3>
-        <p class="text-sm">${item.summary}</p>`;
+  const renderNewsWrapper = (width, content) => {
+    return ` <div class="cell small-12 medium-${width}">
+                <div class="grid-x">
+                    <div class="cell"><h2 class="header-stripped u-header--margin-stripped">News</h2></div>
+                    ${content}
+                </div>
+            </div>`;
   };
+
+  const renderEventsWrapper = (content) => {
+    return ` <div class="cell small-12 medium-4">
+                <div class="grid-x">
+                    <div class="cell"><h2 class="header-stripped u-header--margin-stripped">Events</h2></div>
+                    ${content}
+                </div>
+            </div>`;
+  };
+
+  const renderEvent = (item) => {
+    return `<div class="cell small-12 ">
+                <img class="show-for-medium" src="${item.image}" alt="Image for event: ${item.title}" loading="lazy" />
+                <time class="u-block u-my-1 u-grey--dark">${renderEventDate(item.stirStart)} - ${renderEventDate(item.stirEnd)}</time>
+                <h3 class="header-stripped u-mb-1 u-font-normal u-compress-line-height">
+                    <a href="${item.url}" class="c-link u-inline">${item.title}</a>
+                </h3>
+                <p class="text-sm">${item.summary}</p>
+            </div>`;
+  };
+
+  const renderNewsItem = stir.curry((width, item) => {
+    return `<div class="cell small-12 medium-${width}">
+                <img class="show-for-medium" src="${item.thumbnail}" alt="Image for article: ${item.title}" loading="lazy" />
+                <time class="u-block u-my-1 u-grey--dark">${item.date}</time>
+                <h3 class="header-stripped u-mb-1 u-font-normal u-compress-line-height">
+                    <a href="${item.url}" class="c-link u-inline">${item.title}</a>
+                </h3>
+                <p class="text-sm">${item.summary}</p>
+            </div>`;
+  });
 
   /* Data Processing Helpers */
 
@@ -45,14 +75,24 @@
         return [];
       });
 
-  const initializeEvents = (dataEvents, tag, node) => {
+  // processData
+  const processData = (dataEvents, dataNews, tag, node) => {
     const now = getISONow();
+    const event = stir.compose(stir.map(renderEvent), first, stir.sort(sortByDatetime), stir.filter(filterByTag(tag)), stir.filter(filterUpcomingEvents), stir.map(addIsUpcoming(now)), stir.filter(filterValidEvents))(dataEvents);
 
-    const processEvents = stir.compose(stir.map(renderEvent), first, stir.sort(sortByDatetime), stir.filter(filterByTag(tag)), stir.filter(filterUpcomingEvents), stir.map(addIsUpcoming(now)), stir.filter(filterValidEvents))(dataEvents);
+    //const event = [];
+    const noOfNews = event.length === 1 ? 2 : 3;
+    const newsCellWidth = noOfNews === 2 ? 6 : 4;
+    const newsWrapperWidth = noOfNews === 2 ? 8 : 12;
 
-    node.innerHTML = processEvents.join("");
+    const renderNewsCurry = renderNewsItem(newsCellWidth);
+
+    const news = stir.map(renderNewsCurry, dataNews).slice(0, noOfNews);
+
+    node.innerHTML = renderNewsWrapper(newsWrapperWidth, news.join("")) + renderEventsWrapper(event.join(""));
   };
 
+  // getApiUrl
   const getApiUrl = (env) => {
     const urls = {
       dev: "../index.json",
@@ -65,12 +105,13 @@
   /* Main function */
 
   const main = () => {
-    const node = stir.node("#eventListing");
+    console.log(newsList);
+    const node = stir.node("#newsEventListing");
     const tag = node.dataset.tags;
 
     if (!node || !tag) return;
 
-    fetchEventsData(getApiUrl(UoS_env.name)).then((dataEvents) => initializeEvents(dataEvents, tag, node));
+    fetchEventsData(getApiUrl(UoS_env.name)).then((dataEvents) => processData(dataEvents, newsList, tag, node));
   };
 
   main();
