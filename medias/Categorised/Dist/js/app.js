0aad496d9b7e4c8ea94333f0ce9c99643bbdef9e
Ryan Kaye <ryankaye@gmail.com>
2024-11-21 15:09:14 +0000

    Rename new gallery to aviod conflicts

--- medias/Categorised/Dist/js/app.js
+++ medias/Categorised/Dist/js/app.js
@@ -4023,7 +4023,7 @@ stir.accord = (function () {
 
 var stir = stir || {};
 (function () {
-  const scriptSrc = UoS_env.name.includes("preview") ? '<t4 type="media" id="183454" formatter="path/*" />' : UoS_env.wc_path + "js/other/gallery.js";
+  const scriptSrc = UoS_env.name.includes("preview") ? '<t4 type="media" id="185628" formatter="path/*" />' : UoS_env.wc_path + "js/other/micro-gallery.js";
   //stir.addScript(scriptSrc);
   stir.lazyJS([".stir-microgallery"], scriptSrc);
 })();
