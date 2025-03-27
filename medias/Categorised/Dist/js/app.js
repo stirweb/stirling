e35144e689b6c7e8d1273391a5c8b65547bb7174
Robert Morrison <r.w.morrison@stir.ac.uk>
2025-03-27 17:37:42 +0000

    regulp

--- medias/Categorised/Dist/js/app.js
+++ medias/Categorised/Dist/js/app.js
@@ -1100,27 +1100,20 @@ var QueryParams = (function () {
    * @param {boolean} replace [Optional] Use replaceState instead of pushState
    * @param {boolean} noencode [Optional] Don't use encodeURIComponent on name parameter [default = false]
    */
-  // TODO use document..search? instead of href
   function _remove(name, reload, url, replace, noencode) {
-    if (!url) url = window.location.href;
 
-	/* old code here */
-	// var newUrl = _removeURLParameter(url, noencode? name : encodeURIComponent(name));
-	/* old code end */
-	
-	/* new code here */
-	var newUrl = url;
-	if(url.indexOf(name)>-1) {
-		newUrl = _removeURLParameter(url, name);
-	} else if (url.indexOf(encodeURIComponent(name))>-1) {
-		newUrl = _removeURLParameter(url, encodeURIComponent(name));
-	}
-	/* new code end */
+    const u = new URL(url||window.location);
 
-	if(newUrl!==url) replace ? _replaceStateHandler(newUrl): _pushStateHandler(newUrl);
-
-    if (reload) {
-      window.location.href = document.location.href;
+    if (u.searchParams.has(name)) {
+      u.searchParams.delete(name);
+      if (replace) {
+        _replaceStateHandler(u.href)
+      } else {
+        _pushStateHandler(u.href);
+      }
+      if (reload) {
+        window.location.href = document.location.href;
+      }
     }
   }
 
