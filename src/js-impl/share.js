087781d6af96561aa6d56c2d115196cb0d95cdc4
Robert Morrison <r.w.morrison@stir.ac.uk>
2025-08-08 10:43:00 +0100

    improve share sheet

--- src/js-impl/share.js
+++ src/js-impl/share.js
@@ -5,26 +5,30 @@ stir.share = (()=>{
 	const button = document.querySelector('[data-open="shareSheet"]');
 	if(!button) return;
 
+	const scripts = {
+		dev:'/medias/Categorised/Dist/js/other/share.js',
+		'appdev-preview': '<t4 type="media" id="190308" formatter="path/*" />',
+		preview: '<t4 type="media" id="192023" formatter="path/*" />',
+		prod: '<t4 type="media" id="192023" formatter="path/*" />'
+	};
+
 	function _getShareData(el) {
 		return {
-			title: document.head.querySelector('[property="og:title"]')?.getAttribute('content')||document.head.title,
-			url: document.head.querySelector('[property="og:url"]')?.getAttribute('content')||document.location.href,
+			title: (el&&el.hasAttribute("data-title")&&el.getAttribute("data-title"))||document.head.querySelector('[property="og:title"]')?.getAttribute('content')||document.head.querySelector("title").textContent,
+			url: (el&&el.hasAttribute("data-url")&&el.getAttribute("data-url"))||document.head.querySelector('[property="og:url"]')?.getAttribute('content')||document.location.href,
 			description: document.head.querySelector('[property="og:description"],[name="description"]')?.getAttribute('content'),
-			type: (document.head.querySelector('[name="stir.type"]')?.getAttribute('content')||"").toLowerCase(),
+			entity: (document.head.querySelector('[name="stir.type"]')?.getAttribute('content')||"").toLowerCase(),
 			image: document.head.querySelector('[name="og:image"],[property="og:image"]')?.getAttribute('content')
 		};
 	}
 
 	if (navigator.share) {
 		button.addEventListener("click", async (e) => {
-			try {
-				await navigator.share(_getShareData(e.target));
-			} catch (error) {
-				//console.error(error.message);
-			}
+			try { await navigator.share(_getShareData(e.target)); }
+			catch (error) { /* console.error(error.message); */ }
 		});
 	} else {
-		stir.addScript("dev"!==UoS_env.name?'<t4 type="media" id="192023" formatter="path/*" />':'/medias/Categorised/Dist/js/other/share.js');
+		stir.addScript(scripts[UoS_env.name]||'');
 	}
 
 	return {
--- src/js-other-2/share.js
+++ src/js-other-2/share.js
@@ -4,11 +4,16 @@ stir.share = stir.share || {};
 
 stir.share.fallback = (() => {
 
+	const debug = UoS_env.name !== "prod";
+
 	const icons = {
 		close: '<span aria-hidden=true>×</span>',
 		copy: '<svg viewBox="0 0 50 50"><path fill="currentColor" d="M46.8,13.6H30.6V3.3c0-0.7-0.5-1.2-1.2-1.2H12c-0.3,0-0.6,0.1-0.9,0.4l-8.7,8.7C2.1,11.4,2,11.7,2,12v23.3 c0,0.7,0.5,1.2,1.2,1.2h16.2v10.3c0,0.7,0.5,1.2,1.2,1.2h26.1c0.7,0,1.2-0.5,1.2-1.2v-32C48,14.1,47.5,13.6,46.8,13.6z M10.7,6.3 v4.4H6.3L10.7,6.3z M19.7,22.7c-0.2,0.2-0.3,0.5-0.3,0.8v10.6h-15V13.2H12c0.7,0,1.2-0.5,1.2-1.2V4.5h15v9.8L19.7,22.7z M28.1,17.8 v4.4h-4.4L28.1,17.8z M45.6,45.6H21.9V24.8h7.5c0.6,0,1.1-0.4,1.2-1h0v-7.7h15V45.6z"/></svg>',
-		bsky: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M111.8 62.2C170.2 105.9 233 194.7 256 242.4c23-47.6 85.8-136.4 144.2-180.2c42.1-31.6 110.3-56 110.3 21.8c0 15.5-8.9 130.5-14.1 149.2C478.2 298 412 314.6 353.1 304.5c102.9 17.5 129.1 75.5 72.5 133.5c-107.4 110.2-154.3-27.6-166.3-62.9l0 0c-1.7-4.9-2.6-7.8-3.3-7.8s-1.6 3-3.3 7.8l0 0c-12 35.3-59 173.1-166.3 62.9c-56.5-58-30.4-116 72.5-133.5C100 314.6 33.8 298 15.7 233.1C10.4 214.4 1.5 99.4 1.5 83.9c0-77.8 68.2-53.4 110.3-21.8z"/><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--></svg>',
-		x: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1227"><path fill="currentColor" d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"></path></svg>'
+		bs: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M111.8 62.2C170.2 105.9 233 194.7 256 242.4c23-47.6 85.8-136.4 144.2-180.2c42.1-31.6 110.3-56 110.3 21.8c0 15.5-8.9 130.5-14.1 149.2C478.2 298 412 314.6 353.1 304.5c102.9 17.5 129.1 75.5 72.5 133.5c-107.4 110.2-154.3-27.6-166.3-62.9l0 0c-1.7-4.9-2.6-7.8-3.3-7.8s-1.6 3-3.3 7.8l0 0c-12 35.3-59 173.1-166.3 62.9c-56.5-58-30.4-116 72.5-133.5C100 314.6 33.8 298 15.7 233.1C10.4 214.4 1.5 99.4 1.5 83.9c0-77.8 68.2-53.4 110.3-21.8z"/><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--></svg>',
+		x:  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1227"><path fill="currentColor" d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"></path></svg>',
+		li: '<span class="uos-linkedin"></span>',
+		fb: '<span class="uos-facebook"></span>',
+		wb: '<span class="uos-weibo"></span>'
 	};
 
 	const template = data => {
@@ -17,7 +22,7 @@ stir.share.fallback = (() => {
 			title: encodeURIComponent(data.title)
 		};
 		return `
-		<h3 class="header-stripped">Share this ${data.entity||'page'}</h3>
+		<p class=u-font-bold>Share this ${data.entity||'page'}</p>
 		<div class="copy-link-box">
 			<div>
 				<p>${data.title}</p>
@@ -26,29 +31,23 @@ stir.share.fallback = (() => {
 			<div class=tooltip__link-copied>Link copied to clipboard</div>
 		</div>
 		<ul class="barnacles">
-			<li title="Copy to clipboard">
+			<li title="Copy this link to your clipboard">
 				<a href="#copy" data-copy="${data.url}">${icons.copy}</a>
 			</li>
 			<li title="Share with Facebook">
-				<a href="https://www.facebook.com/sharer.php?u=${enc.url}" title="Share this page on Facebook" class="fbc-has-badge fbc-UID_1">
-					<span class="uos-facebook"></span>
-				</a>
+				<a href="https://www.facebook.com/sharer.php?u=${enc.url}" title="Share this page on Facebook">${icons.fb}</a>
 			</li>
 			<li title="Share with LinkedIn">
-				<a href="https://www.linkedin.com/shareArticle?url=${enc.url}&amp;title=${enc.title}" title="Share this page on LinkedIn">
-					<span class="uos-linkedin"></span>
-				</a>
+				<a href="https://www.linkedin.com/shareArticle?url=${enc.url}&amp;title=${enc.title}" title="Share this page on LinkedIn">${icons.li}</a>
 			</li>
 			<li title="Share with Bluesky">
-				<a href="https://bsky.app/intent/compose?text=${enc.title}%0A${enc.url}" title="Share this page on Bluesky">${icons.bsky}</a>
+				<a href="https://bsky.app/intent/compose?text=${enc.title}%0A${enc.url}" title="Share this page on Bluesky">${icons.bs}</a>
 			</li>
 			<li title="Share with X the everything app™">
 				<a href="https://x.com/intent/tweet?url=${enc.url}&amp;text=${enc.title}&amp;via=StirUni" title="Share this page on X">${icons.x}</a>
 			</li>
 			<li title="Share with Weibo">
-				<a href="http://service.weibo.com/share/share.php?url=${enc.url}&amp;title=${enc.title}" title="Share this page on Weibo">
-					<span class="uos-weibo"></span>
-				</a>
+				<a href="http://service.weibo.com/share/share.php?url=${enc.url}&amp;title=${enc.title}" title="Share this page on Weibo">${icons.wb}</a>
 			</li>
 		</ul>
 		`;
@@ -75,6 +74,7 @@ stir.share.fallback = (() => {
 		const thumb = document.createElement('img');
 		thumb.src = shareData.image;
 		thumb.alt = shareData.description;
+		thumb.onerror = imageError;
 		copylinkbox.prepend(thumb);
 	}
 
@@ -89,6 +89,11 @@ stir.share.fallback = (() => {
 		copy.focus();
 		event.preventDefault();
 	})
+
+	function imageError(event) {
+		debug && console.info("[Image] loading error - element removed", this);
+		this.src && this.parentElement.removeChild(this);
+	}
 
 	function copyTextToClipboard(text) {
 		if (!text) return console.error("Copy error: nothing to copy.");
