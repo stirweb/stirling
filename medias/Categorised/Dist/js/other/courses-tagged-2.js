edc579da48e4e3dc48ff04bf59c5a897310f3baf
Ryan Kaye <ryankaye@gmail.com>
2025-10-02 10:31:14 +0100

    Courses tagged - Fix for empty filter tag

--- medias/Categorised/Dist/js/other/courses-tagged-2.js
+++ medias/Categorised/Dist/js/other/courses-tagged-2.js
@@ -1,4 +1,4 @@
-!function(t){if(!t)return;"localhost"!=window.location.hostname&&window.location.hostname;const o=t=>stir.map(t=>({name:t[0],value:t[1].trim()}),t),l=(t,e,s,r,i)=>{var l=Object.entries(e.dataset),a=[{name:"subject",value:i}],l=o(l),l=stir.filter(t=>""!==t.value,l),a=[...l,...a],a=stir.filter(t=>""!==t.value,a),r=stir.filter(t=>t.title,r),l=stir.filter(n(l),r),r=stir.filter(n(a),l),a=l.map(t=>t.subject).join(", ").split(", "),l=stir.removeDuplicates(a).sort(),a=(p(s,u(l,i)),t.dataset.render||""),s=stir.sort((t,e)=>t.title>e.title?1:e.title>t.title?-1:0),l=c(a),i=p(e);stir.compose(i,l,s)(r)},n=stir.curry((t,s)=>{const r=t=>t;return stir.all(r,stir.map(e=>{var t=stir.map(t=>-1!==e.value.indexOf(t.trim()),s[e.name].split(","));return stir.any(r,t)},t))}),c=stir.curry((t,e)=>("compact"===t?stir.map(s,e):stir.map(r,e)).join("")),u=(t,e)=>`<option value="">Filter by subject...</option>
+!function(t){if(!t)return;"localhost"!=window.location.hostname&&window.location.hostname;const l=(t,e,s,r,i)=>{var l=Object.entries(e.dataset),a=[{name:"subject",value:i}],l=p(l),l=stir.filter(t=>""!==t.value,l),a=[...l,...a],a=stir.filter(t=>""!==t.value,a),r=stir.filter(t=>t.title,r),l=stir.filter(o(l),r),r=stir.filter(o(a),l),a=l.map(t=>t.subject).join(", ").split(", "),l=stir.removeDuplicates(a).sort(),a=(u(s,c(l,i)),t.dataset.render||""),s=stir.sort((t,e)=>t.title>e.title?1:e.title>t.title?-1:0),l=n(a),i=u(e);stir.compose(i,l,s)(r)},o=stir.curry((t,s)=>{const r=t=>t;return stir.all(r,stir.map(e=>{var t=s[e.name].split(",").map(t=>""!==t.trim()&&-1!==e.value.indexOf(t.trim()));return stir.any(r,t)},t))}),n=stir.curry((t,e)=>("compact"===t?stir.map(s,e):stir.map(r,e)).join("")),c=(t,e)=>`<option value="">Filter by subject...</option>
 			`+t.map(t=>`<option value="${t}" ${t===e?"selected":""}>${t}</option>`).join(""),s=t=>`
 			<div class="cell small-12 medium-4 u-pt-2">
 			  <div class="u-green-line-top">
@@ -13,8 +13,8 @@
           <p class="text-sm"><strong>${t.starts} </strong></p>
           <p class="text-sm">${t.description}</p>
         </div>
-			</div>`,p=stir.curry((t,e)=>(stir.setHTML(t,e),!0)),a=stir.courses||[];a.length&&t.forEach(t=>{{var e=t,s=a;t=e.querySelector(".filtersbox");const r=e.querySelector(".resultbox"),i=(p(t,`
+			</div>`,u=stir.curry((t,e)=>(stir.setHTML(t,e),!0)),p=t=>stir.map(t=>({name:t[0],value:t[1].trim()}),t),a=stir.courses||[];a.length&&t.forEach(t=>{{var e=t,s=a;t=e.querySelector(".filtersbox");const r=e.querySelector(".resultbox"),i=(u(t,`
           <label class="show-for-sr" for="subjectsfilter">Filter by subject</label>
           <select id="subjectsfilter">
 			        <option value="">Filter by subject...</option>
-			      </select>`),t.querySelector("select"));return i&&i.addEventListener("change",t=>{t.preventDefault();t=t.target.options[t.target.selectedIndex].value;return l(e,r,i,s,t)}),l(e,r,i,s,"")}})}(stir.nodes(".courselisting"));
+			    </select>`),t.querySelector("select"));return i&&i.addEventListener("change",t=>{t.preventDefault();t=t.target.options[t.target.selectedIndex].value;return l(e,r,i,s,t)}),l(e,r,i,s,"")}})}(stir.nodes(".courselisting"));
