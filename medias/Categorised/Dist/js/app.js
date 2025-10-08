f75735d1c4c742753c76309eea84d257a300d356
Ryan Kaye <ryankaye@gmail.com>
2025-10-08 09:51:32 +0100

    Personalisation: UI amends

--- medias/Categorised/Dist/js/app.js
+++ medias/Categorised/Dist/js/app.js
@@ -3828,12 +3828,12 @@ var stir = stir || {};
 (function () {
   const STORAGE_KEY = "stirsess";
   const MAX_AGE_MS = 24 * 60 * 60 * 1000; // 24 hours
-  const SERVER_PATH = UoS_env.name === `§` ? "/research/hub/test/personalisation/server.php" : "server.php";
+  const SERVER_PATH = UoS_env.name === `prod` ? "/research/hub/test/personalisation/server.php" : "server.php";
   const JAN_COURSES_PATH = UoS_env.name === `prod` ? "/data/courses/pg/json/january-starts/index.json" : "./january-starts.json";
   const CLOSING_DATE = new Date("2025-11-14T23:59:59");
   const DEV_MODE = UoS_env.name === `dev`;
   const PROD_MODE = UoS_env.name === `prod`;
-  const MAX_SHOWS = UoS_env.name === `prod` ? 3 : 1000;
+  const MAX_SHOWS = UoS_env.name === `prod` ? 2 : 1000;
 
   const EXEMPT_LIST = ["/courses/"];
 
@@ -3908,7 +3908,7 @@ var stir = stir || {};
    */
   function renderMessage(scholarship, region) {
     if (!scholarship) return " ";
-    return `Up to <b>${scholarship.value}</b> in scholarships are available to you`;
+    return `<b>${scholarship.value}</b> ${region} scholarship`;
   }
 
   /**
@@ -3948,59 +3948,26 @@ var stir = stir || {};
     const event = data[0];
     //const daysLeft = Math.ceil((closingDate - Date.now()) / (1000 * 60 * 60 * 24));
     const humanDate = closingDate.toLocaleDateString("en-GB", { month: "long", day: "numeric" });
-    /*
-    const html2 = `<div class="grid-x grid-container">
-                    <div class="u-my-2 cell">
-                        <div class="grid-x flex-dir-column medium-flex-dir-row u-p-2 u-m-0 c-wrapper-2025 purples ">
-                            <p class="cell small-12 large-8 u-m-0 ">
-                                <span class="text-md  "><b><a href="${event.p}">${event.prefix} ${event.title}</a></b></span><br>
-                                <span class="text-lg u-text-coloured "><b>January application deadline ${humanDate}</b></span><br>
-                                ${renderMessage(scholarship, event.n)}
-                            </p>
-                            <p class="cell small-12 large-4 u-m-0"> <a class="button expanded" href="${event.p}">Apply now</a></p>
-                        </div>
-                    </div>
-                </div>`;
 
     const html = `<div class="grid-x grid-container">
                     <div class="u-my-2 cell">
                         <div class="grid-x flex-dir-column align-middle medium-flex-dir-row u-p-2 u-m-0 c-wrapper-2025 purples u-border-left-solid u-border-width-5 u-border-coloured">
                             <p class="cell small-12 large-8 u-m-0 ">
-                                <span class="text-lg u-text-coloured u-mb-1 u-inline-block"><b>Welcome back</b></span><br>
-                                Apply by <b>${humanDate}</b> to start your course in January, or check out
-                                our September entry options. Up to ${renderMessage(scholarship, event.n)}.
+                                <span class="text-lg u-text-coloured u-mb-1 u-inline-block"><b>Still interested in starting a Masters in January 2026?</b></span><br>
+                                Apply by <b>${humanDate}</b> - ${renderMessage(scholarship, event.n)}<br /> Or check out our courses starting September 2026
                             </p>
                             <div class="cell small-12 large-4 "> 
-                                  <div class="u-relative purples" >
+                                  <div class="u-relative purples u-mt-2-medium-down" >
                                     <button class="button button--dropdown expanded " type="button" data-toggle="dd-personalisation-janstarts"
-                                        id="btn-personalisation-janstarts">Apply now</button>
+                                        id="btn-personalisation-janstarts">Apply for </button>
                                     <div class="dropdown-pane u-absolute purples dark u-px-2" id="dd-personalisation-janstarts" data-dropdown="" data-auto-focus="true">
                                         <ul>
-                                          ${data.map((event) => `<li><a href="${event.p}" class="c-link  ">${event.prefix} ${event.title}</a></li>`).join("")}   
-                                        </ul>
-                                    </div>
-                                  </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>`;*/
-
-    const html = `<div class="grid-x grid-container">
-                    <div class="u-my-2 cell">
-                        <div class="grid-x flex-dir-column align-middle medium-flex-dir-row u-p-2 u-m-0 c-wrapper-2025 purples u-border-left-solid u-border-width-5 u-border-coloured">
-                            <p class="cell small-12 large-8 u-m-0 ">
-                                <span class="text-lg u-text-coloured u-mb-1 u-inline-block"><b>Still interested in postgraduate study?</b></span><br>
-                                ${renderMessage(scholarship, event.n)}. Apply by <b>${humanDate}</b> to start in January, or check out our September options.
-                            </p>
-                            <div class="cell small-12 large-4 "> 
-                                  <div class="u-relative purples" >
-                                    <button class="button button--dropdown expanded " type="button" data-toggle="dd-personalisation-janstarts"
-                                        id="btn-personalisation-janstarts">Apply for...</button>
-                                    <div class="dropdown-pane u-absolute purples dark u-px-2" id="dd-personalisation-janstarts" data-dropdown="" data-auto-focus="true">
-                                        <ul>
-                                          ${data.map((event) => `<li><a href="${event.p}" class="c-link  ">${event.prefix} ${event.title}</a></li>`).join("")}   
-                                          <li><a href="/courses/pg-taught/" class="c-link  ">View all postgraduate courses</a></li>
+                                          ${data
+                                            .map(
+                                              (event) => `<li><a href="${event.p}" class="c-link text-sm link-personalisation-janstarts">${event.prefix} ${event.title}</a></li>`
+                                            )
+                                            .join("")}   
+                                          <li><a href="/courses/pg-taught/" class="c-link text-sm link-personalisation-janstarts">View all postgraduate courses</a></li>
                                         </ul>
                                     </div>
                                   </div>
@@ -4009,7 +3976,6 @@ var stir = stir || {};
                         </div>
                     </div>
                 </div>`;
-
     return html;
   }
 
@@ -4024,7 +3990,7 @@ var stir = stir || {};
     try {
       const parsed = JSON.parse(stored);
       if (!parsed.ts) return true;
-      const age = Date.now() - new Date(parsed.ts).getTime();
+      const age = Date.now() - new Date(parsed.ts).getTime(); // in ms
       return age > maxAgeMS;
     } catch {
       return true;
@@ -4188,7 +4154,7 @@ var stir = stir || {};
 
     if (shouldFetch(stored, MAX_AGE_MS)) {
       // Fetch from the server
-      console.log("Fetching from server...");
+      //console.log("Fetching from server...");
       const parsed = await fetchAndStoreData(aid, STORAGE_KEY, SERVER_PATH, 1);
       if (!parsed.length) return;
 
@@ -4205,7 +4171,7 @@ var stir = stir || {};
       PROD_MODE && dataLayer.push({ event: "personalisation-janstarts" });
     } else {
       // Use cached data
-      console.log("Fetching data from cookie...");
+      //console.log("Fetching data from cookie...");
       const parsed = JSON.parse(stored);
       if (!parsed.data.length) return;
 
