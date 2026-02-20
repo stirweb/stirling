0844972597af35e25451d4e47648c9ae70c11db1
Robert Morrison <r.w.morrison@stir.ac.uk>
2026-02-20 11:43:08 +0000

    create stir.Suggest

---
+++ src/js-impl/autosuggest.js
@@ -0,0 +1,15 @@
+/**
+ * SEARCH AUTO-SUGGEST
+ * @author: Robert Morrison <r.w.morrison@stir.ac.uk>
+ */
+
+// we will add some new modules to the stir library
+var stir = stir || {};
+
+/**
+ * Concierge
+ * Instantiated below with `new stir.Concierge();`
+ */
+stir.Suggester = function Suggester(popup) {
+  if (!stir.addSearch) return;
+};
