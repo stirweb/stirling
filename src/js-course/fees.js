d68f49ab4134bf9f149bb9e784e0cb145f4009e9
Robert Morrison <r.w.morrison@stir.ac.uk>
2026-03-05 10:09:36 +0000

    fee table templates UG & PG

--- src/js-course/fees.js
+++ src/js-course/fees.js
@@ -2,91 +2,6 @@ var stir = stir || {};
 stir.t4Globals = stir.t4Globals || {};
 stir.fees = stir.fees || {};
 
-stir.fees.template = {
-	chooser: `<h4>Select your fee status to see the tuition fee for this course:</h4>`,
-	default: `<option value disabled selected>Select fee status</option>`
-};
-
-stir.fees.doFeesTable = function doFeesTable (scope) {    
-	if (!scope) return;
-	const label  = document.createElement('label');
-	const select  = document.createElement('select');
-	const table   = document.createElement('table');
-	var remotes = Array.prototype.slice.call(scope.querySelectorAll('[data-action="change-region"]'));
-	var region;
-
-    label.innerHTML = stir.fees.template.chooser;
-    select.innerHTML = stir.fees.template.default;
-    label.append(select);
-	scope.prepend(table);
-	scope.prepend(label);
-
-	function toggle(flag) {
-		if (this.nodeType === 1)
-			flag ? this.classList.remove('hide') : this.classList.add('hide');
-	}
-
-	function show(el) {
-		toggle.call(el, true);
-	}
-
-	function hide(el) {
-		toggle.call(el, false)
-	}
-
-	function hideAll() {
-		hide(table);
-		getRegionals().forEach(hide); // IE Compatible forEach
-	}
-
-	function getRegionals(region) {
-		// querySelectorAll returns a NodeList, but IE can't use forEach() on
-		// a NodeList directly, so this function converts it to a regular
-		// Array, which is more compatible.
-		return Array.prototype.slice.call(scope.querySelectorAll('[data-region' + (region ? '="' + region + '"' : '') + ']'));
-	}
-
-	function handleChanges() {
-		// First, hide all region-specific elements:
-		hideAll();
-		// Then, only reveal the ones that match the selected region.
-		if (region = this.options[this.options.selectedIndex].value) {
-			showTheStuff(region);
-		}
-	}
-
-	function showTheStuff(region) {
-		show(table);
-		getRegionals(region).forEach(show);
-	}
-
-	// Initial state: hide the table and all region-specific elements (until
-	// the user has selected a region):
-	hideAll();
-
-	// Now listen for the user:
-	if(!select.id) select.id = 'change-region';
-	select.addEventListener('change', handleChanges);
-
-	// Set up any remote controls. Each `remote` should just be
-	// a simple <span> with some text:
-	remotes.forEach(function(remote, i) {
-		var a = document.createElement('a');						// create a new <a> element
-		remote.childNodes && a.appendChild(remote.childNodes[0]);	// move the text node (if it exists) into the link
-		remote.appendChild(a);										// then move the <a> into the DOM where the text was
-		a.setAttribute("tabindex", "0");							//	
-		a.setAttribute("href", "#");								//	required attributes for keyboard a11y
-		a.setAttribute("aria-controls", select.id);
-
-		a.addEventListener("click", function(event) {
-			select.value = this.parentNode.getAttribute('data-value');
-			select.dispatchEvent(new Event("change"));
-			event.preventDefault();
-			select.focus();
-		});
-	});
-};
-
 /**
  * Fees region (e.g. home/eu) selector
  * @param {*} scope DOM element that wraps the fees information (selector and table, etc).
@@ -106,52 +21,40 @@ stir.fees.doFeesTable = function doFeesTable (scope) {
 	}
 	
 	let initialised = false;
-	const stuff = {};
-	stuff.feestab = document.querySelector('[data-tab-callback="stir.fees.auto"] + div [data-behaviour="accordion"] div');
 	const info = {};
 	const feeapi = "dev"===UoS_env.name?'../fees.json':'<t4 type="media" id="182818" formatter="path/*" />'
 	// Media #182818 is "fees.json" which contains a T4 Web Object that fetches live data via the live site.
-	// (in preview it will make an API call, in staging it will be "t4-cached").
+	// (in preview it will make an API call, in production it will be "t4-cached").
 
-	const labels = {
-		UG: {
-			"H": "Scotland",
-			"R": "England, Wales, NI, Republic of Ireland",
-			"O": "International (including EU)",
-		},
-		PG: {
-			"H": "UK and Republic of Ireland",
-			"O": "International (including EU)",
-		}
-	};
+	// const labels = {
+	// 	UG: {
+	// 		"H": "Scotland",
+	// 		"R": "England, Wales, NI, Republic of Ireland",
+	// 		"O": "International (including EU)",
+	// 	},
+	// 	PG: {
+	// 		"H": "UK and Republic of Ireland",
+	// 		"O": "International (including EU)",
+	// 	}
+	// };
 
-	const statuses = {
-		UG: {
-			"H": "Scottish students",
-			"R": "Students from England, Wales, Northern Ireland and Republic of Ireland",
-			"O": "International students (including EU)",
-		},
-		PG: {
-			"H": "Students from the UK and Republic of Ireland",
-			"O": "International (including EU) students",
-		}
-	};
-	const regions = {
-		UG: {
-			"H": "home",
-			"R": "ruk",
-			"O": "int-eu",
-		},
-		PG: {
-			"H": "home",
-			"O": "overseas",
-		}
-	};
-	const modes = {
-		"FT":"full time",
-		"PTO":"part time",
-		"SW":"sandwich"
-	}
+	// const statuses = {
+	// 	UG: {
+	// 		"H": "Scottish students",
+	// 		"R": "Students from England, Wales, Northern Ireland and Republic of Ireland",
+	// 		"O": "International students (including EU)",
+	// 	},
+	// 	PG: {
+	// 		"H": "Students from the UK and Republic of Ireland",
+	// 		"O": "International (including EU) students",
+	// 	}
+	// };
+	
+// 	const modes = {
+// 		"FT":"full time",
+// 		"PTO":"part time",
+// 		"SW":"sandwich"
+// 	}
 
 	const formatter = new Intl.NumberFormat('en-GB', {
 		style: 'currency',
@@ -163,23 +66,7 @@ stir.fees.doFeesTable = function doFeesTable (scope) {
 		//maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 	  });
 
-	const feetables = data => {
-		
-		return feetable(
-			info.stata.map(status=>
-				info.moda.map(mode => 
-					feetablerow(status,mode,
-						data.feeData.filter(
-							a=>a.feeStatus===status&&a.modeOfAttendance===mode
-						)
-					)
-				).join('')
-			).join(''),"Annual fees");
-	};
-
 	const onlyUnique = (value, index, self)  => self.indexOf(value) === index;
-
-
 
 	const th_year = year => `<th scope="col" style="width:20%;">${(year)}</th>`;
 	const td_amount = data => `<td>${formatter.format(data.amount)}</td>`;
@@ -216,25 +103,37 @@ stir.fees.doFeesTable = function doFeesTable (scope) {
 		
 		routes && stir.getJSON(feeapi, data=>{
 			if(data.feeData) {
+				debug && console.info('[Fees] data loaded',data);
 				const route = routes.shift();
 				routedata = data.feeData.filter(item=>item.rouCode===route);
 				feedata = routedata.length && routedata[0].feeData
+				debug && console.info('[Fees] route',route,routedata);
+				debug && console.info('[Fees] feedata',feedata);
 		
 				info.theyears = feedata && feedata.map(data=>data.academicYear).filter(onlyUnique);
 				info.stata = feedata && feedata.map(data=>data.feeStatus).filter(onlyUnique);
 				info.moda = feedata && feedata.map(data=>data.modeOfAttendance).filter(onlyUnique);
-		
+
+				debug && console.info('[Fees] info',info);
+				
 				if(!routedata.length) {
-					debug && console.error(`[Fee API] ${route}: no match for this route code found in the fees data`);
-					stuff.select && stuff.select.remove();
+					debug && console.error(`[Fee API] ${route}: no match for this route code found in the fees data`);	
 				} else {
 					debug && console.info(`${route}: API fee data ${feedata.length>0?'available':'not available'}`);
 					if(feedata.length) {
-						updateOldTable(routedata.map(feetables).join(''));
-						stuff.select && updateOldSelect();
-					} else {
-						stuff.select && stuff.select.remove();
-					}
+						scope.querySelectorAll("table").forEach(table => {
+							const tr = table.querySelector("thead tr");
+							info.theyears.forEach(year => tr.insertAdjacentHTML("beforeend",th_year(year)));
+							table.querySelectorAll("tbody tr[data-status][data-mode]").forEach(row => {
+								const status = row.getAttribute("data-status");
+								const mode   = row.getAttribute("data-mode");
+								info.theyears.forEach(year => {
+									const fee = feedata.filter(a=>a.feeStatus===status&&a.modeOfAttendance===mode&&a.academicYear===year);
+									row.insertAdjacentHTML("beforeend",fee.map(td_amount).join(''));
+								});
+							});
+						});
+					} else { }
 				}
 			}
 		});
