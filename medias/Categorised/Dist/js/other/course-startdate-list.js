8f753a52fef0c94618699861e15d1ded3a041579
Ryan Kaye <ryankaye@gmail.com>
2026-05-21 11:52:38 +0100

    January Starts UG - fix for multiple apply codes

--- medias/Categorised/Dist/js/other/course-startdate-list.js
+++ medias/Categorised/Dist/js/other/course-startdate-list.js
@@ -1,11 +1,9 @@
-!function(t){if(t)if(stir.feeds&&stir.feeds.data){const e=stir.curry(t=>{return`
+!function(t){if(t)if(stir.feeds&&stir.feeds.data){const a=stir.curry(a=>{var t=(a.portalapply?a.portalapply.split(", ").map(t=>t.trim()):[]).map((t,r)=>`<a href="https://portal.stir.ac.uk/student/course-application/ugd/application.jsp?crsCode=${t}">${a.prefix.split(" / ")[r]} ${a.title}</a>`).join(", ");return`
                <tr>
                    <td>
-                   ${t.portalapply?`<a href="https://portal.stir.ac.uk/student/course-application/ugd/application.jsp?crsCode=${t.portalapply}">`:""}
-                   ${t.prefix} ${t.title}
-                   ${t.portalapply?"</a>":""}
+                    ${a.portalapply?t:""}
                    </td>
-                   <td>${t.janfull}</td>
+                   <td>${a.janfull}</td>
                </tr>`});var r=stir.curry(t=>`
              <table>
                  <caption>Full time courses starting in January</caption>
@@ -13,6 +11,6 @@
                      <tr><th>Course</th><th>Year of entry</th></tr>
                  </thead>
                  <tbody>
-                     ${t.map(t=>e(t)).join("")}
+                     ${t.map(t=>a(t)).join("")}
                  </tbody>
              </table>`)(stir.feeds.data.filter(t=>t.janfull&&0<Object.keys(t.janfull).length).sort((t,r)=>t.title.localeCompare(r.title)));t&&(t.innerHTML=r)}else console.error("Course data feed not found")}(stir.node("#course-list"));
